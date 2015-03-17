5a69f98b65f784c1a87105132ec472ef46f6cb43
deaddog <mikkelslsl@gmail.com>
2015-03-17 21:49:33 +0100

    An ArgumentParser can manage one or more callback methods.

--- ArgumentParser.cs
+++ ArgumentParser.cs
@@ -8,5 +8,21 @@ namespace CommandLineParsing
 {
     public class ArgumentParser<T>
     {
+        private Action<T> callback;
+
+        internal ArgumentParser()
+        {
+            this.callback = null;
+        }
+
+        public ArgumentParser<T> Callback(Action<T> callback)
+        {
+            if (this.callback == null)
+                this.callback = callback;
+            else
+                this.callback += callback;
+
+            return this;
+        }
     }
 }
