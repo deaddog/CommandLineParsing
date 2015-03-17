fac571d4cae2933280fb7cb1c7f1b2dde6430c58
deaddog <mikkelslsl@gmail.com>
2015-03-17 21:53:18 +0100

    Type validation can be set for an ArgumentParser. An error message can be supplied (optional).

--- ArgumentParser.cs
+++ ArgumentParser.cs
@@ -8,10 +8,13 @@ namespace CommandLineParsing
 {
     public class ArgumentParser<T>
     {
+        private Func<string, Message> typeValidator;
+
         private Action<T> callback;
 
         internal ArgumentParser()
         {
+            this.typeValidator = null;
             this.callback = null;
         }
 
@@ -22,6 +25,23 @@ namespace CommandLineParsing
             else
                 this.callback += callback;
 
+            return this;
+        }
+
+        public ArgumentParser<T> ValidateType()
+        {
+            return ValidateType(x => ("Argument " + x + "could not be parsed to a value of type " + typeof(T).Name + "."));
+        }
+        public ArgumentParser<T> ValidateType(Message errorMessage)
+        {
+            return ValidateType(x => errorMessage);
+        }
+        public ArgumentParser<T> ValidateType(Func<string, Message> errorMessage)
+        {
+            if (this.typeValidator != null)
+                throw new InvalidOperationException("Type validation can only be applied to " + this.GetType().Name + " once.");
+
+            this.typeValidator = errorMessage;
             return this;
         }
     }
