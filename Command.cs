32ed9e1037e8adf37e3176c5a26c744e884139b5
deaddog <mikkelslsl@gmail.com>
2015-03-18 21:49:41 +0100

    Added a range of default TryParse methods for various types.

--- Command.cs
+++ Command.cs
@@ -30,10 +30,48 @@ namespace CommandLineParsing
 
         private ParseWrapper<T> getTryParse<T>()
         {
+            if (typeof(T) == typeof(byte))
+                return new ParseWrapper<byte>(byte.TryParse) as ParseWrapper<T>;
+            if (typeof(T) == typeof(sbyte))
+                return new ParseWrapper<sbyte>(sbyte.TryParse) as ParseWrapper<T>;
+            if (typeof(T) == typeof(short))
+                return new ParseWrapper<short>(short.TryParse) as ParseWrapper<T>;
+            if (typeof(T) == typeof(ushort))
+                return new ParseWrapper<ushort>(ushort.TryParse) as ParseWrapper<T>;
             if (typeof(T) == typeof(int))
                 return new ParseWrapper<int>(int.TryParse) as ParseWrapper<T>;
+            if (typeof(T) == typeof(uint))
+                return new ParseWrapper<uint>(uint.TryParse) as ParseWrapper<T>;
+            if (typeof(T) == typeof(long))
+                return new ParseWrapper<long>(long.TryParse) as ParseWrapper<T>;
+            if (typeof(T) == typeof(ulong))
+                return new ParseWrapper<ulong>(ulong.TryParse) as ParseWrapper<T>;
+
+            if (typeof(T) == typeof(float))
+                return new ParseWrapper<float>(float.TryParse) as ParseWrapper<T>;
+            if (typeof(T) == typeof(double))
+                return new ParseWrapper<double>(double.TryParse) as ParseWrapper<T>;
+            if (typeof(T) == typeof(decimal))
+                return new ParseWrapper<decimal>(decimal.TryParse) as ParseWrapper<T>;
+
+            if (typeof(T) == typeof(bool))
+                return new ParseWrapper<bool>(bool.TryParse) as ParseWrapper<T>;
+
+            if (typeof(T) == typeof(DateTime))
+                return new ParseWrapper<DateTime>(DateTime.TryParse) as ParseWrapper<T>;
+
+            if (typeof(T) == typeof(char))
+                return new ParseWrapper<char>(char.TryParse) as ParseWrapper<T>;
+            if (typeof(T) == typeof(string))
+                return new ParseWrapper<string>(tryParseString) as ParseWrapper<T>;
 
             throw new NotSupportedException("The type " + typeof(T) + " is not supported.");
+        }
+
+        private bool tryParseString(string s, out string result)
+        {
+            result = s;
+            return true;
         }
 
         private class ParseWrapper<T>
