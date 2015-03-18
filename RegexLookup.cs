fb044b3c5772ef6915163fbd9f5a5b03a34e68ef
deaddog <mikkelslsl@gmail.com>
2015-03-18 22:33:46 +0100

    Created a static class for listing usefull regular expressions.

---
+++ RegexLookup.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CommandLineParsing
+{
+    internal static class RegexLookup
+    {
+        public static readonly Regex ArgumentName = new Regex("^--?[a-z]+(-[a-z]+)*$");
+    }
+}
