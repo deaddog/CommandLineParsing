171ac8c40b4a0ecff80e112d9afd764c6dc2c588
deaddog <mikkelslsl@gmail.com>
2015-03-18 16:37:18 +0100

    Provided the implementation of the Handle method in both argument parsers.

--- ArrayArgumentParser.cs
+++ ArrayArgumentParser.cs
@@ -8,5 +8,25 @@ namespace CommandLineParsing
 {
     public class ArrayArgumentParser<T> : ArgumentParser<T[]>
     {
+        private TryParse<T> parser;
+
+        public ArrayArgumentParser(string name, TryParse<T> parser)
+            : base(name, null)
+        {
+            this.parser = parser;
+        }
+
+        internal override Message Handle(Argument argument)
+        {
+            T[] values = new T[argument.Count];
+
+            for (int i = 0; i < argument.Count; i++)
+            {
+                if (!parser(argument[i], out values[i]))
+                    return doTypeValidation(argument[i]);
+            }
+
+            return doValidationAndCallback(values);
+        }
     }
 }
--- SingleArgumentParser.cs
+++ SingleArgumentParser.cs
@@ -8,5 +8,26 @@ namespace CommandLineParsing
 {
     public class SingleArgumentParser<T> : ArgumentParser<T>
     {
+        private TryParse<T> parser;
+
+        public SingleArgumentParser(string name, TryParse<T> parser)
+            : base(name, parser)
+        {
+            this.parser = parser;
+        }
+
+        internal override Message Handle(Argument argument)
+        {
+            T value;
+
+            if (argument.Count == 0)
+                return new Message("No value provided for argument \"" + argument + "\".");
+            else if (argument.Count > 1)
+                return new Message("Only one value can be provided for argument \"" + argument + "\".");
+            else if (!parser(argument[0], out value))
+                return doTypeValidation(argument[0]);
+
+            return doValidationAndCallback(value);
+        }
     }
 }
