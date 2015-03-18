ae47c18648b2ee98260fa9e4f6d63e62629be6a7
deaddog <mikkelslsl@gmail.com>
2015-03-18 14:23:56 +0100

    Created an Argument class for storing arguments and their associated values.

---
+++ Argument.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CommandLineParsing
+{
+    public class Argument
+    {
+        private string key;
+        private string[] values;
+
+        public Argument(string key, IEnumerable<string> values)
+        {
+            this.key = key.ToLower();
+            this.values = values.ToArray();
+        }
+
+        public string Key
+        {
+            get { return key; }
+        }
+
+        public int Count
+        {
+            get { return values.Length; }
+        }
+
+        public string this[int index]
+        {
+            get { return values[index]; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} [{1}]", key, string.Join(", ", values));
+        }
+    }
+}
