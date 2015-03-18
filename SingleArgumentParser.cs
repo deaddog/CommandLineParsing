4d5ee4422f3cec53468732f5470367436c3b155e
deaddog <mikkelslsl@gmail.com>
2015-03-18 17:48:58 +0100

    The SingleArgumentParser allows for setting error message for no-value and multiple-values.

--- SingleArgumentParser.cs
+++ SingleArgumentParser.cs
@@ -9,11 +9,15 @@ namespace CommandLineParsing
     public class SingleArgumentParser<T> : ArgumentParser<T, SingleArgumentParser<T>>
     {
         private TryParse<T> parser;
+        private Func<string, Message> noValueMessage;
+        private Func<string, Message> multipleValuesMessage;
 
         internal SingleArgumentParser(string name, TryParse<T> parser)
             : base(name)
         {
             this.parser = parser;
+            this.noValueMessage = x => "No value provided for argument \"" + x + "\".";
+            this.multipleValuesMessage = x => "Only one value can be provided for argument \"" + x + "\".";
         }
 
         internal override Message Handle(Argument argument)
@@ -21,13 +25,33 @@ namespace CommandLineParsing
             T value;
 
             if (argument.Count == 0)
-                return new Message("No value provided for argument \"" + argument.Key + "\".");
+                return noValueMessage(argument.Key);
             else if (argument.Count > 1)
-                return new Message("Only one value can be provided for argument \"" + argument.Key + "\".");
+                return multipleValuesMessage(argument.Key);
             else if (!parser(argument[0], out value))
                 return doTypeValidation(argument[0]);
 
             return doValidationAndCallback(value);
+        }
+
+        public SingleArgumentParser<T> NoValue(Message errorMessage)
+        {
+            return NoValue(x => errorMessage);
+        }
+        public SingleArgumentParser<T> NoValue(Func<string, Message> errorMessage)
+        {
+            noValueMessage = errorMessage;
+            return this;
+        }
+
+        public SingleArgumentParser<T> MultipleValues(Message errorMessage)
+        {
+            return MultipleValues(x => errorMessage);
+        }
+        public SingleArgumentParser<T> MultipleValues(Func<string, Message> errorMessage)
+        {
+            multipleValuesMessage = errorMessage;
+            return this;
         }
     }
 }
