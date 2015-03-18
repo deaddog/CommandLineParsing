18a694d3de15ed7ca981db5b64240176344a8f01
deaddog <mikkelslsl@gmail.com>
2015-03-18 17:53:55 +0100

    Provided an implementation of Command that allows for adding arguments. Currently only int-type arguments are supported.

--- Command.cs
+++ Command.cs
@@ -8,5 +8,39 @@ namespace CommandLineParsing
 {
     public class Command
     {
+        private Dictionary<string, ArgumentParser> arguments;
+
+        public Command()
+        {
+            this.arguments = new Dictionary<string, ArgumentParser>();
+        }
+
+        public SingleArgumentParser<T> Argument<T>(string name)
+        {
+            var parser = getParser<T>(name);
+            arguments.Add(name, parser);
+            return parser;
+        }
+        private SingleArgumentParser<T> getParser<T>(string name)
+        {
+            if (typeof(T) == typeof(int))
+                return new SingleArgumentParser<int>(name, int.TryParse) as SingleArgumentParser<T>;
+
+            throw new NotSupportedException("The type " + typeof(T) + " is not supported.");
+        }
+
+        public ArrayArgumentParser<T> ArrayArgument<T>(string name)
+        {
+            var parser = getArrayParser<T>(name);
+            arguments.Add(name, parser);
+            return parser;
+        }
+        public ArrayArgumentParser<T> getArrayParser<T>(string name)
+        {
+            if (typeof(T) == typeof(int))
+                return new ArrayArgumentParser<int>(name, int.TryParse) as ArrayArgumentParser<T>;
+
+            throw new NotSupportedException("The type " + typeof(T) + " is not supported.");
+        }
     }
 }
