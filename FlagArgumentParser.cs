4716862bb2ae4d11919d7a9a4722f9d15388fdbd
deaddog <mikkelslsl@gmail.com>
2015-03-19 23:09:16 +0100

    FlagArgumentParser provides a method for displaying an error, when a value is supplied with the argument.

--- FlagArgumentParser.cs
+++ FlagArgumentParser.cs
@@ -11,15 +11,21 @@ namespace CommandLineParsing
         private string description;
         private Action callback;
 
+        private Func<string, Message> hasValueMessage;
+
         public FlagArgumentParser(string name)
             : base(name)
         {
             this.description = null;
             this.callback = null;
+
+            this.hasValueMessage = x => x + " is a flag argument, it does not support values.";
         }
 
         internal override Message Handle(Argument argument)
         {
+            if (argument.Count > 0)
+                    return hasValueMessage(argument.Key);
             if (callback != null)
                 callback();
 
@@ -38,6 +44,16 @@ namespace CommandLineParsing
         public FlagArgumentParser Callback(Action callback)
         {
             this.callback += callback;
+            return this;
+        }
+
+        public FlagArgumentParser HasValue(Message errorMessage)
+        {
+            return HasValue(x => errorMessage);
+        }
+        public FlagArgumentParser HasValue(Func<string, Message> errorMessage)
+        {
+            hasValueMessage = errorMessage;
             return this;
         }
 
