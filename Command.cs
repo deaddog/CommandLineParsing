16c8e08f9375ae7463b5430db0e55aff44190238
deaddog <mikkelslsl@gmail.com>
2015-03-19 19:47:29 +0100

    Command maintains a static dictionary of known parsers, such that reflection will only be used once per type.

--- Command.cs
+++ Command.cs
@@ -88,7 +88,7 @@ namespace CommandLineParsing
 
         public SingleArgumentParser<T> Argument<T>(string name, params string[] alternatives)
         {
-            var parser = new SingleArgumentParser<T>(name, getParser<T>());
+            var parser = new SingleArgumentParser<T>(name, parserTable.GetParser<T>());
             parsers.Add(parser);
 
             if (!RegexLookup.ArgumentName.IsMatch(name))
@@ -106,7 +106,7 @@ namespace CommandLineParsing
         }
         public ArrayArgumentParser<T> ArrayArgument<T>(string name, params string[] alternatives)
         {
-            var parser = new ArrayArgumentParser<T>(name, getParser<T>());
+            var parser = new ArrayArgumentParser<T>(name, parserTable.GetParser<T>());
             parsers.Add(parser);
 
             if (!RegexLookup.ArgumentName.IsMatch(name))
@@ -123,49 +123,73 @@ namespace CommandLineParsing
             return parser;
         }
 
+        private static ParserDictionary parserTable = new ParserDictionary();
+        #region Implementation of parser retrievel
 
-
-        private TryParse<T> getParser<T>()
+        private class ParserDictionary
         {
-            if (typeof(T) == typeof(string))
-                return wrapParser<string, T>(tryParseString);
+            private Dictionary<Type, Delegate> known;
 
-            var type = typeof(T);
-            if (type.IsEnum)
-                return getParserEnum<T>();
+            public ParserDictionary()
+            {
+                known = new Dictionary<Type, Delegate>();
+            }
 
-            var refType = type.MakeByRefType();
+            public TryParse<T> GetParser<T>()
+            {
+                Delegate parser;
+                if (!known.TryGetValue(typeof(T), out parser))
+                {
+                    parser = getParser<T>();
+                    known.Add(typeof(T), parser);
+                }
+                return parser as TryParse<T>;
+            }
 
-            var methods = from m in type.GetMethods()
-                          where m.Name == "TryParse" && m.IsStatic && !m.IsGenericMethod && m.ReturnType == typeof(bool)
-                          let par = m.GetParameters()
-                          where par.Length == 2 && par[0].ParameterType == typeof(string) && par[1].IsOut && par[1].ParameterType == refType
-                          select m;
+            private static TryParse<T> getParser<T>()
+            {
+                if (typeof(T) == typeof(string))
+                    return wrapParser<string, T>(tryParseString);
 
-            var method = methods.FirstOrDefault();
-            if (method == null)
-                throw new NotSupportedException("The type " + typeof(T) + " is not supported. It must provide a static non-generic implementation of the TryParse delegate.");
-            else
-                return method.CreateDelegate(typeof(TryParse<T>)) as TryParse<T>;
+                var type = typeof(T);
+                if (type.IsEnum)
+                    return getParserEnum<T>();
+
+                var refType = type.MakeByRefType();
+
+                var methods = from m in type.GetMethods()
+                              where m.Name == "TryParse" && m.IsStatic && !m.IsGenericMethod && m.ReturnType == typeof(bool)
+                              let par = m.GetParameters()
+                              where par.Length == 2 && par[0].ParameterType == typeof(string) && par[1].IsOut && par[1].ParameterType == refType
+                              select m;
+
+                var method = methods.FirstOrDefault();
+                if (method == null)
+                    throw new NotSupportedException("The type " + typeof(T) + " is not supported. It must provide a static non-generic implementation of the TryParse delegate.");
+                else
+                    return method.CreateDelegate(typeof(TryParse<T>)) as TryParse<T>;
+            }
+            private static TryParse<T> getParserEnum<T>()
+            {
+                var methods = from m in typeof(Enum).GetMethods()
+                              where m.Name == "TryParse" && m.IsGenericMethod
+                              select m;
+
+                return methods.First().MakeGenericMethod(typeof(T)).CreateDelegate(typeof(TryParse<T>)) as TryParse<T>;
+            }
+
+            private static bool tryParseString(string s, out string result)
+            {
+                result = s;
+                return true;
+            }
+
+            private static TryParse<TTo> wrapParser<TFrom, TTo>(TryParse<TFrom> parser)
+            {
+                return parser as TryParse<TTo>;
+            }
         }
-        private static TryParse<T> getParserEnum<T>()
-        {
-            var methods = from m in typeof(Enum).GetMethods()
-                          where m.Name == "TryParse" && m.IsGenericMethod
-                          select m;
 
-            return methods.First().MakeGenericMethod(typeof(T)).CreateDelegate(typeof(TryParse<T>)) as TryParse<T>;
-        }
-
-        private bool tryParseString(string s, out string result)
-        {
-            result = s;
-            return true;
-        }
-
-        private static TryParse<TTo> wrapParser<TFrom, TTo>(TryParse<TFrom> parser)
-        {
-            return parser as TryParse<TTo>;
-        }
+        #endregion
     }
 }
