01b686d608b0238c1b0ebb9ef282539cbcbd6410
deaddog <mikkelslsl@gmail.com>
2015-03-21 00:16:30 +0100

    Created two specializations of Parameter. A generic type for managing values and one for flag parameters.

---
+++ FlagParameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParsing
+{
+    public class FlagParameter : Parameter
+    {
+        internal FlagParameter(string name, string description)
+            : base(name, description)
+        {
+        }
+    }
+}
