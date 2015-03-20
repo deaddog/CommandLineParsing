66c88f7025c9fbfbd45bba1366c18900f30c9321
deaddog <mikkelslsl@gmail.com>
2015-03-21 00:13:20 +0100

    Created a Parameter class definition.

---
+++ Parameter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParsing
+{
+    public class Parameter
+    {
+        private string name;
+        private string description;
+
+        internal Parameter(string name, string description)
+        {
+            this.name = name;
+            this.description = description;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+        public string Description
+        {
+            get { return description; }
+        }
+    }
+}
