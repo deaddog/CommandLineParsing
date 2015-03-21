9b16e8826df22ed375447622e7eff5e188531e3d
deaddog <mikkelslsl@gmail.com>
2015-03-21 20:58:13 +0100

    Added an IsSet property (getter) and a HasValueMessage property to FlagParameter.

--- FlagParameter.cs
+++ FlagParameter.cs
@@ -8,11 +8,28 @@ namespace CommandLineParsing
 {
     public class FlagParameter : Parameter
     {
+        private Message hasValueMessage;
+        private bool isset;
+
         internal FlagParameter(string name, string description, Message required)
             : base(name, description, required)
         {
             if (this.IsRequired)
                 throw new ArgumentException("A FlagParameter cannot be required.", "required");
+
+            isset = false;
+            hasValueMessage = name + " is a flag argument, it does not support values.";
+        }
+
+        public bool IsSet
+        {
+            get { return isset; }
+        }
+
+        public Message HasValueMessage
+        {
+            get { return hasValueMessage; }
+            set { hasValueMessage = value; }
         }
 
         internal override Message Handle(Argument argument)
