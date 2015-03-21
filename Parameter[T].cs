f179ca9367fa35dc4ce1f83c455a9bb562d79fcc
deaddog <mikkelslsl@gmail.com>
2015-03-22 00:38:54 +0100

    Parameter<> parses input and returns a type-error message on parsing error.

--- Parameter[T].cs
+++ Parameter[T].cs
@@ -11,11 +11,15 @@ namespace CommandLineParsing
         protected T value;
         private TryParse<T> parser;
 
+        private Func<string, Message> typeErrorMessage;
+
         internal Parameter(string name, string description, Message required)
             : base(name, description, required)
         {
             this.value = default(T);
             this.parser = null;
+
+            this.typeErrorMessage = x => string.Format("Argument \"{0}\" with value \"{1}\" could not be parsed to a value of type {2}.", Name, x, typeof(T).Name);
         }
 
         public virtual T Value
@@ -23,10 +27,30 @@ namespace CommandLineParsing
             get { return value; }
         }
 
+        public Func<string, Message> TypeErrorMessage
+        {
+            get { return typeErrorMessage; }
+            set
+            {
+                if (typeErrorMessage == null)
+                    throw new ArgumentNullException("value");
+
+                typeErrorMessage = value;
+            }
+        }
+
         internal override Message Handle(Argument argument)
         {
             if (parser == null)
                 parser = ParserLookup.Table.GetParser<T>();
+
+            T temp;
+
+            if (!parser(argument[0], out temp))
+                return typeErrorMessage(argument[0]);
+
+            value = temp;
+            doCallback();
 
             return Message.NoError;
         }
