d6a53e35ea91f0ac92e1d9905ea1611f4bbfee9b
deaddog <mikkelslsl@gmail.com>
2015-03-21 15:19:53 +0100

    When initializing a Command, all Parameter fields are found using reflection.

--- Command.Reflection.cs
+++ Command.Reflection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,37 @@ namespace CommandLineParsing
     {
         private void initializeParameters()
         {
-            throw new NotImplementedException();
+            var fields = getParameterFields();
+        }
+        private FieldInfo[] getParameterFields()
+        {
+            var fields = this.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+            var accepted = new List<FieldInfo>();
+
+            for (int i = 0; i < fields.Length; i++)
+            {
+                if (!isFieldTypeParameter(fields[i].FieldType))
+                    continue;
+
+                if (!fields[i].IsInitOnly)
+                    throw new FieldAccessException(string.Format("{0} fields must be defined as readonly - '{1}' in {2} is not.", typeof(Parameter).Name, fields[i].Name, fields[i].DeclaringType));
+
+                if (fields[i].IsStatic)
+                    throw new FieldAccessException(string.Format("The {0} field '{1}' in {2} is defined as static.", typeof(Parameter).Name, fields[i].Name, fields[i].DeclaringType));
+
+                accepted.Add(fields[i]);
+            }
+
+            return accepted.ToArray();
+        }
+        private bool isFieldTypeParameter(Type fieldType)
+        {
+            if (fieldType == typeof(FlagParameter))
+                return true;
+            else if (fieldType.IsGenericType)
+                return fieldType.GetGenericTypeDefinition() == typeof(Parameter<>);
+            else
+                return false;
         }
     }
 }
