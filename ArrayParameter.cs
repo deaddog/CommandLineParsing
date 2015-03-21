e876804ffb0af398cea47b615fdf0d4cc68dbf20
deaddog <mikkelslsl@gmail.com>
2015-03-21 22:46:31 +0100

    Created a ArrayParameter class for handling parameters with arrayed data.

---
+++ ArrayParameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParsing
+{
+    internal class ArrayParameter<T> : Parameter<T[]>
+    {
+        internal ArrayParameter(string name, string description, Message required)
+            : base(name, description, required)
+        {
+        }
+    }
+}
