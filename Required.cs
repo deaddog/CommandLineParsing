43aea9230b5de6c19814720e614e98d6c8be0fee
deaddog <mikkelslsl@gmail.com>
2015-03-21 15:56:02 +0100

    Created a Required attribute, that signals if a parameter is required for the command to work.

---
+++ Required.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParsing
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+    public class Required : Attribute
+    {
+        internal readonly Message message;
+
+        public Required()
+        {
+            this.message = null;
+        }
+        public Required(string message)
+        {
+            if (message == null)
+                throw new ArgumentNullException("message");
+
+            this.message = message;
+        }
+
+        internal static Message defaultMessage(string name)
+        {
+            return "No value specified for the required parameter \"" + name + "\"";
+        }
+    }
+}
