33f44ff65f7db63627c38aabf90ceedefe73e998
deaddog <mikkelslsl@gmail.com>
2015-03-21 15:26:45 +0100

    Created a Name attribute.

---
+++ Name.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CommandLineParsing
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+    public sealed class Name : Attribute
+    {
+        private string[] names;
+
+        public Name(string name, params string[] alternatives)
+        {
+            this.names = new string[1 + alternatives.Length];
+            this.names[0] = name;
+            alternatives.CopyTo(this.names, 1);
+        }
+
+        public string[] Names
+        {
+            get { return names; }
+        }
+    }
+}
