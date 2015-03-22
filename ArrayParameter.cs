7198b4d0e7267c097b129bbea82db2584a717a84
deaddog <mikkelslsl@gmail.com>
2015-03-22 13:57:50 +0100

    Implemented the Handle method in ArrayParameter.

--- ArrayParameter.cs
+++ ArrayParameter.cs
@@ -8,9 +8,40 @@ namespace CommandLineParsing
 {
     internal class ArrayParameter<T> : Parameter<T[]>
     {
+        private TryParse<T> parser;
+
         internal ArrayParameter(string name, string description, Message required)
             : base(name, description, required)
         {
+            this.parser = null;
+        }
+
+        public override T[] Value
+        {
+            get { return value.ToArray(); }
+        }
+
+        internal override Message Handle(Argument argument)
+        {
+            if (parser == null) 
+                parser = ParserLookup.Table.GetParser<T>();
+
+            T[] temp = new T[argument.Count];
+
+            for (int i = 0; i < argument.Count; i++)
+            {
+                if (!parser(argument[i], out temp[i]))
+                    return TypeErrorMessage(argument[i]);
+            }
+
+            var msg = doValidation(temp);
+            if (msg.IsError)
+                return msg;
+
+            value = temp;
+            doCallback();
+
+            return Message.NoError;
         }
     }
 }
