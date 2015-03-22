64443a05464f02f6d0ddc6ca136a16dedbbf47f4
deaddog <mikkelslsl@gmail.com>
2015-03-22 17:12:08 +0100

    Reimplemented the ValidateEach methods originally provided by ArrayArgumentParser.

--- ValidationExtension.cs
+++ ValidationExtension.cs
@@ -16,5 +16,27 @@ namespace CommandLineParsing
         {
             parameter.Validate(x => validator(x) ? Message.NoError : errorMessage);
         }
+
+        public static void ValidateEach<T>(this Parameter<T[]> parameter, Func<T, Message> validator)
+        {
+            parameter.Validate(x =>
+            {
+                for (int i = 0; i < x.Length; i++)
+                {
+                    var msg = validator(x[i]);
+                    if (msg.IsError)
+                        return msg;
+                }
+                return Message.NoError;
+            });
+        }
+        public static void ValidateEach<T>(this Parameter<T[]> parameter, Func<T, bool> validator, Func<T, Message> errorMessage)
+        {
+            parameter.ValidateEach(x => validator(x) ? Message.NoError : errorMessage(x));
+        }
+        public static void ValidateEach<T>(this Parameter<T[]> parameter, Func<T, bool> validator, Message errorMessage)
+        {
+            parameter.ValidateEach(x => validator(x) ? Message.NoError : errorMessage);
+        }
     }
 }
