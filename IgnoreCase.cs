3275f4bdcd0a2d6be0d3a428a9d83c69a01f78dc
deaddog <mikkelslsl@gmail.com>
2015-03-22 20:26:24 +0100

    Created a IgnoreCase attribute.

---
+++ IgnoreCase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParsing
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+    public class IgnoreCase : Attribute
+    {
+        public IgnoreCase()
+        {
+        }
+    }
+}
