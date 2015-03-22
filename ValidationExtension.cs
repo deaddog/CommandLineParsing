c406846a72aa0bc43909d9f583215ebbbf2380e5
deaddog <mikkelslsl@gmail.com>
2015-03-22 17:10:02 +0100

    Reimplemented the Validate overrides originally provided by ValueArgumentParser.

--- ValidationExtension.cs
+++ ValidationExtension.cs
@@ -8,6 +8,13 @@ namespace CommandLineParsing
 {
     public static class ValidationExtension
     {
-        
+        public static void Validate<T>(this Parameter<T> parameter, Func<T, bool> validator, Func<T, Message> errorMessage)
+        {
+            parameter.Validate(x => validator(x) ? Message.NoError : errorMessage(x));
+        }
+        public static void Validate<T>(this Parameter<T> parameter, Func<T, bool> validator, Message errorMessage)
+        {
+            parameter.Validate(x => validator(x) ? Message.NoError : errorMessage);
+        }
     }
 }
