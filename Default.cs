42c342f7b0a4bc08f6756294d5e2c22afabca034
deaddog <mikkelslsl@gmail.com>
2015-03-23 08:10:59 +0100

    Created a Default attribute class.

---
+++ Default.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParsing
+{
+    /// <summary>
+    /// Specifies the default value for a parameter.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+    public class Default : Attribute
+    {
+        private readonly object value;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Default"/> class.
+        /// </summary>
+        /// <param name="value">The default value for the parameter.
+        /// This must be a type that can be cast to the type of the parameter.</param>
+        public Default(object value)
+        {
+            this.value = value;
+        }
+
+        internal object Value
+        {
+            get { return value; }
+        }
+    }
+}
