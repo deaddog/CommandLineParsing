f4e70828c66af656a47e363f855fe71d9780d3e0
deaddog <mikkelslsl@gmail.com>
2015-03-27 18:24:21 +0100

    Created a new Attribute 'NoName' for identifying a single parameter that cathes argument values without keys.

---
+++ NoName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CommandLineParsing
+{
+    /// <summary>
+    /// Specifies that a parameter is used unnamed.
+    /// Note that there can only be one such parameter and that the parameter type must be an array.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+    public class NoName : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NoName"/> class.
+        /// </summary>
+        public NoName()
+        {
+        }
+    }
+}
