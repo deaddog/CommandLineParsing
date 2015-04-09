ac5fd63e9554c34c794f70a0651af72c0a4600c8
deaddog <mikkelslsl@gmail.com>
2015-04-09 17:57:36 +0200

    Removed the old parsing methods from the Argument class.

--- Argument.cs
+++ Argument.cs
@@ -9,40 +9,6 @@ namespace CommandLineParsing
         private string key;
         private string[] values;
 
-        public static Stack<Argument> Parse(string[] args)
-        {
-            return new Stack<Argument>(parse(args).Reverse());
-        }
-        private static IEnumerable<Argument> parse(string[] args)
-        {
-            string key = null;
-            List<string> values = new List<string>();
-
-            foreach (var a in args)
-            {
-                bool isKey = RegexLookup.ArgumentName.IsMatch(a);
-
-                if (key == null && !isKey)
-                    yield return new Argument(a, new string[0]);
-
-                else if (isKey)
-                {
-                    if (key != null)
-                    {
-                        yield return new Argument(key, values);
-                        key = null;
-                        values.Clear();
-                    }
-                    key = a;
-                }
-                else
-                    values.Add(a);
-            }
-
-            if (key != null)
-                yield return new Argument(key, values);
-        }
-
         public Argument(IEnumerable<string> values)
         {
             this.key = null;
