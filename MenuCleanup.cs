2121cf9642a10ebe8f359f099f4bca2951645a47
deaddog <mikkelslsl@gmail.com>
2015-04-12 00:11:37 +0200

    Added documentation for the MenuCleanup enum.

--- MenuCleanup.cs
+++ MenuCleanup.cs
@@ -2,10 +2,24 @@
 
 namespace CommandLineParsing
 {
+    /// <summary>
+    /// Defines what type of console cleanup should be applied after displaying a menu.
+    /// </summary>
     public enum MenuCleanup
     {
+        /// <summary>
+        /// None cleanup is applied.
+        /// </summary>
         None,
+        /// <summary>
+        /// The menu is removed from the console.
+        /// After an option has been selected, no part of the menu will remain visible.
+        /// </summary>
         RemoveMenu,
+        /// <summary>
+        /// The menu is removed from the console.
+        /// After an option has been selected, only the selected option will remain visible.
+        /// </summary>
         RemoveMenuShowChoice
     }
 }
