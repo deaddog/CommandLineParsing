fcf6de22e4d0d0c83fef895ee9d59389a742b3bb
deaddog <mikkelslsl@gmail.com>
2015-04-11 19:42:32 +0200

    Provided documentation for the generic Parameter class, except for the validation method.

--- Parameter[T].cs
+++ Parameter[T].cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace CommandLineParsing
 {
+    /// <summary>
+    /// Represents a <see cref="Parameter"/> that is associated with a value type.
+    /// A <see cref="Parameter{T}"/> will read a value of type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the value that is accepted for the <see cref="Parameter"/>.
+    /// This type can be any type that has a static <see cref="TryParse{T}"/> method, an array of such a type or any enumeration type.</typeparam>
     public class Parameter<T> : Parameter
     {
+#pragma warning disable 1591
+
         protected T value;
         protected bool isDefault;
 
@@ -31,6 +39,8 @@ namespace CommandLineParsing
             return Message.NoError;
         }
 
+#pragma warning restore
+
         internal Parameter(string name, string[] alternatives, string description, Message required, bool enumIgnore)
             : base(name, alternatives, description, required)
         {
@@ -47,15 +57,33 @@ namespace CommandLineParsing
             this.validators = new List<Func<T, Message>>();
         }
 
+        /// <summary>
+        /// Gets the value parsed by this <see cref="Parameter{T}"/>, or the default value if <see cref="IsDefault"/> is <c>true</c>.
+        /// </summary>
+        /// <value>
+        /// The value parsed by this <see cref="Parameter{T}"/>.
+        /// </value>
         public virtual T Value
         {
             get { return value; }
         }
+        /// <summary>
+        /// Gets a value indicating whether the <see cref="Value"/> property holds the default value.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this <see cref="Parameter{T}"/> is not provided for the executed command; otherwise, <c>false</c>.
+        /// </value>
         public bool IsDefault
         {
             get { return isDefault; }
         }
 
+        /// <summary>
+        /// Sets the default value for this <see cref="Parameter{T}"/>.
+        /// This method replaces any value set by previous invocations of the method and any value set using the <see cref="Default"/> attribute.
+        /// Unlike the <see cref="Default"/> attribute, this methods allows for using an expression as the default value.
+        /// </summary>
+        /// <param name="value">The default value of this <see cref="Parameter{T}"/> if the parameter is not used when executing its containing command.</param>
         public void SetDefault(T value)
         {
             if (this.IsRequired)
@@ -65,6 +93,14 @@ namespace CommandLineParsing
             this.isDefault = true;
         }
 
+        /// <summary>
+        /// Gets or sets the function that is used to generate type error messages for this <see cref="Parameter{T}"/>.
+        /// Type error messages, are messages where the input string could not be parsed by the appropriate <see cref="TryParse{T}"/> method.
+        /// The <see cref="string"/> parameter for the function is the string that the <see cref="Parameter{T}"/> could not parse.
+        /// </summary>
+        /// <value>
+        /// The function that generates type error messages for this <see cref="Parameter{T}"/>.
+        /// </value>
         public Func<string, Message> TypeErrorMessage
         {
             get { return typeErrorMessage; }
@@ -76,6 +112,13 @@ namespace CommandLineParsing
                 typeErrorMessage = value;
             }
         }
+        /// <summary>
+        /// Gets or sets the <see cref="Message"/> returned when no value is provided for this <see cref="Parameter{T}"/>.
+        /// This <see cref="Message"/> is not applied when <typeparamref name="T"/> is an array.
+        /// </summary>
+        /// <value>
+        /// The no value message.
+        /// </value>
         public Message NoValueMessage
         {
             get { return noValueMessage; }
@@ -89,6 +132,13 @@ namespace CommandLineParsing
                 this.noValueMessage = value;
             }
         }
+        /// <summary>
+        /// Gets or sets the <see cref="Message"/> returned when multiple values are provided for this <see cref="Parameter{T}"/>.
+        /// This <see cref="Message"/> is not applied when <typeparamref name="T"/> is an array.
+        /// </summary>
+        /// <value>
+        /// The multiple values message.
+        /// </value>
         public Message MultipleValuesMessage
         {
             get { return multipleValuesMessage; }
@@ -144,6 +194,12 @@ namespace CommandLineParsing
             return parser(value, out temp);
         }
 
+        /// <summary>
+        /// Returns a <see cref="string"/> that represents this <see cref="Parameter{T}"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string"/> that represents this instance.
+        /// </returns>
         public override string ToString()
         {
             return string.Format("{0}[{1}] = {2}{3}",
