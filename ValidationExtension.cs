2131616361179dae93091db53966ee2b7c41b11f
deaddog <mikkelslsl@gmail.com>
2015-04-11 21:53:06 +0200

    Rewrote the validation extension methods, to extend the Validator<T> type instead of the Parameter<T> type.

--- ValidationExtension.cs
+++ ValidationExtension.cs
@@ -9,44 +9,44 @@ namespace CommandLineParsing
 {
     /// <summary>
     /// Provides a collection of validation methods for parameters, as extension methods.
-    /// These methods are all based on the <see cref="Parameter{T}.Validate"/> method.
+    /// These methods are all based on the <see cref="Validator{T}.Validate"/> method.
     /// </summary>
     public static class ValidationExtension
     {
         /// <summary>
-        /// Provides a validation method for the <see cref="Parameter{T}"/>.
+        /// Provides a validation method for this <see cref="Validator{T}"/>.
         /// </summary>
-        /// <typeparam name="T">The value type of the <see cref="Parameter{T}"/>.</typeparam>
-        /// <param name="parameter">The <see cref="Parameter{T}"/> to which the validation should be applied.</param>
+        /// <typeparam name="T">The value type of the <see cref="Validator{T}"/>.</typeparam>
+        /// <param name="validatorElement">The <see cref="Validator{T}"/> to which the validation should be applied.</param>
         /// <param name="validator">A function that takes the parsed value as input and returns <c>true</c> if the value is valid; otherwise is must return <c>false</c>.</param>
         /// <param name="errorMessage">A function that generates the error message that should be the validation result if <paramref name="validator"/> returns <c>false</c>.</param>
-        public static void Validate<T>(this Parameter<T> parameter, Func<T, bool> validator, Func<T, Message> errorMessage)
+        public static void Validate<T>(this Validator<T> validatorElement, Func<T, bool> validator, Func<T, Message> errorMessage)
         {
-            parameter.Validate(x => validator(x) ? Message.NoError : errorMessage(x));
+            validatorElement.Add(x => validator(x) ? Message.NoError : errorMessage(x));
         }
         /// <summary>
-        /// Provides a validation method for the <see cref="Parameter{T}"/>.
+        /// Provides a validation method for the <see cref="Validator{T}"/>.
         /// </summary>
-        /// <typeparam name="T">The value type of the <see cref="Parameter{T}"/>.</typeparam>
-        /// <param name="parameter">The <see cref="Parameter{T}"/> to which the validation should be applied.</param>
+        /// <typeparam name="T">The value type of the <see cref="Validator{T}"/>.</typeparam>
+        /// <param name="validatorElement">The <see cref="Validator{T}"/> to which the validation should be applied.</param>
         /// <param name="validator">A function that takes the parsed value as input and returns <c>true</c> if the value is valid; otherwise is must return <c>false</c>.</param>
         /// <param name="errorMessage">The error message that should be the validation result if <paramref name="validator"/> returns <c>false</c>.</param>
-        public static void Validate<T>(this Parameter<T> parameter, Func<T, bool> validator, Message errorMessage)
+        public static void Validate<T>(this Validator<T> validatorElement, Func<T, bool> validator, Message errorMessage)
         {
-            parameter.Validate(x => validator(x) ? Message.NoError : errorMessage);
+            validatorElement.Add(x => validator(x) ? Message.NoError : errorMessage);
         }
 
         /// <summary>
-        /// Provides a validation method for the <see cref="Parameter{T}"/> that validates each element in the parsed array.
+        /// Provides a validation method for the <see cref="Validator{T}"/> that validates each element in an array.
         /// </summary>
-        /// <typeparam name="T">The value type of each element in the <see cref="Parameter{T}"/> value array.</typeparam>
-        /// <param name="parameter">The <see cref="Parameter{T}"/> to which the validation should be applied.</param>
-        /// <param name="validator">A function that returns the result of validating a single element in <paramref name="parameter"/>.
+        /// <typeparam name="T">The element type of the <see cref="Validator{T}"/> array.</typeparam>
+        /// <param name="validatorElement">The <see cref="Validator{T}"/> to which the validation should be applied.</param>
+        /// <param name="validator">A function that returns the result of validating a single element in <paramref name="validatorElement"/>.
         /// Use <see cref="Message.NoError"/> to indicate validation success.
         /// If a single element is not validated, the remaining elements are not validated.</param>
-        public static void ValidateEach<T>(this Parameter<T[]> parameter, Func<T, Message> validator)
+        public static void ValidateEach<T>(this Validator<T[]> validatorElement, Func<T, Message> validator)
         {
-            parameter.Validate(x =>
+            validatorElement.Add(x =>
             {
                 for (int i = 0; i < x.Length; i++)
                 {
@@ -58,53 +58,53 @@ namespace CommandLineParsing
             });
         }
         /// <summary>
-        /// Provides a validation method for the <see cref="Parameter{T}"/> that validates each element in the parsed array.
+        /// Provides a validation method for the <see cref="Validator{T}"/> that validates each element in an array.
         /// </summary>
-        /// <typeparam name="T">The value type of each element in the <see cref="Parameter{T}"/> value array.</typeparam>
-        /// <param name="parameter">The <see cref="Parameter{T}"/> to which the validation should be applied.</param>
+        /// <typeparam name="T">The element type of the <see cref="Validator{T}"/> array.</typeparam>
+        /// <param name="validatorElement">The <see cref="Validator{T}"/> to which the validation should be applied.</param>
         /// <param name="validator">A function that validates a single <typeparamref name="T"/> element.
         /// The method should return <c>true</c> if the value is valid; otherwise <c>false</c>.</param>
         /// <param name="errorMessage">A function that takes the value that could not be validated and returns an error message describing the reason for the message.</param>
-        public static void ValidateEach<T>(this Parameter<T[]> parameter, Func<T, bool> validator, Func<T, Message> errorMessage)
+        public static void ValidateEach<T>(this Validator<T[]> validatorElement, Func<T, bool> validator, Func<T, Message> errorMessage)
         {
-            parameter.ValidateEach(x => validator(x) ? Message.NoError : errorMessage(x));
+            validatorElement.ValidateEach(x => validator(x) ? Message.NoError : errorMessage(x));
         }
         /// <summary>
-        /// Provides a validation method for the <see cref="Parameter{T}"/> that validates each element in the parsed array.
+        /// Provides a validation method for the <see cref="Validator{T}"/> that validates each element in an array.
         /// </summary>
-        /// <typeparam name="T">The value type of each element in the <see cref="Parameter{T}"/> value array.</typeparam>
-        /// <param name="parameter">The <see cref="Parameter{T}"/> to which the validation should be applied.</param>
+        /// <typeparam name="T">The element type of the <see cref="Validator{T}"/> array.</typeparam>
+        /// <param name="validatorElement">The <see cref="Validator{T}"/> to which the validation should be applied.</param>
         /// <param name="validator">A function that validates a single <typeparamref name="T"/> element.
         /// The method should return <c>true</c> if the value is valid; otherwise <c>false</c>.</param>
         /// <param name="errorMessage">The error message that should be returned if validation of a single element fails (if <paramref name="validator"/> returns <c>false</c>).</param>
-        public static void ValidateEach<T>(this Parameter<T[]> parameter, Func<T, bool> validator, Message errorMessage)
+        public static void ValidateEach<T>(this Validator<T[]> validatorElement, Func<T, bool> validator, Message errorMessage)
         {
-            parameter.ValidateEach(x => validator(x) ? Message.NoError : errorMessage);
+            validatorElement.ValidateEach(x => validator(x) ? Message.NoError : errorMessage);
         }
 
-        public static void ValidateRegex(this Parameter<string> parameter, string regex, Func<string, Message> errorMessage)
+        public static void ValidateRegex(this Validator<string> validatorElement, string regex, Func<string, Message> errorMessage)
         {
-            ValidateRegex(parameter, new Regex(regex), errorMessage);
+            ValidateRegex(validatorElement, new Regex(regex), errorMessage);
         }
-        public static void ValidateRegex(this Parameter<string> parameter, Regex regex, Func<string, Message> errorMessage)
+        public static void ValidateRegex(this Validator<string> validatorElement, Regex regex, Func<string, Message> errorMessage)
         {
-            parameter.Validate(x => regex.IsMatch(x) ? Message.NoError : errorMessage(x));
+            validatorElement.Add(x => regex.IsMatch(x) ? Message.NoError : errorMessage(x));
         }
-        public static void ValidateRegex(this Parameter<string> parameter, string regex, Message errorMessage)
+        public static void ValidateRegex(this Validator<string> validatorElement, string regex, Message errorMessage)
         {
-            ValidateRegex(parameter, new Regex(regex), errorMessage);
+            ValidateRegex(validatorElement, new Regex(regex), errorMessage);
         }
-        public static void ValidateRegex(this Parameter<string> parameter, Regex regex, Message errorMessage)
+        public static void ValidateRegex(this Validator<string> validatorElement, Regex regex, Message errorMessage)
         {
-            parameter.Validate(x => regex.IsMatch(x) ? Message.NoError : errorMessage);
+            validatorElement.Add(x => regex.IsMatch(x) ? Message.NoError : errorMessage);
         }
-        public static void ValidateRegex(this Parameter<string> parameter, string regex)
+        public static void ValidateRegex(this Validator<string> validatorElement, string regex)
         {
-            ValidateRegex(parameter, new Regex(regex));
+            ValidateRegex(validatorElement, new Regex(regex));
         }
-        public static void ValidateRegex(this Parameter<string> parameter, Regex regex)
+        public static void ValidateRegex(this Validator<string> validatorElement, Regex regex)
         {
-            parameter.Validate(x => regex.IsMatch(x) ? Message.NoError : "The \"" + parameter.Name + "\" parameter value must match the regex: [Cyan:" + regex + "]");
+            validatorElement.Add(x => regex.IsMatch(x) ? Message.NoError : "The string \"" + x + "\" does not match the regex: [Cyan:" + regex + "]");
         }
     }
 }
