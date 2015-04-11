a2642a96ad7ea863af963fc9472e985e1b29982e
deaddog <mikkelslsl@gmail.com>
2015-04-11 21:37:17 +0200

    Created a new Validator type that will be used to unify the various validation methods.

---
+++ Validator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParsing
+{
+    public class Validator<T>
+    {
+        private List<Func<T, Message>> validators;
+
+        public Validator()
+        {
+            this.validators = new List<Func<T, Message>>();
+        }
+    }
+}
