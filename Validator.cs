fd371257031181c3d41122cad5af35f1796572c4
deaddog <mikkelslsl@gmail.com>
2015-04-11 21:38:07 +0200

    Copied the primary validation method from Parameter<T> to the new Validator<T> class.

--- Validator.cs
+++ Validator.cs
@@ -14,5 +14,18 @@ namespace CommandLineParsing
         {
             this.validators = new List<Func<T, Message>>();
         }
+
+        /// <summary>
+        /// Provides a validation method for this <see cref="Validator{T}"/>.
+        /// </summary>
+        /// <param name="validator">A function that validates the parsed <typeparamref name="T"/> value and returns a <see cref="Message"/>.
+        /// If the validation was successful <see cref="Message.NoError"/> should be returned by the method; otherwise an appropriate <see cref="Message"/> should be returned.</param>
+        public void Add(Func<T, Message> validator)
+        {
+            if (validator == null)
+                throw new ArgumentNullException("validator");
+
+            this.validators.Add(validator);
+        }
     }
 }
