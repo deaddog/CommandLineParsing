d13e85f4609c090b8c5051edf2339ae44555a661
deaddog <mikkelslsl@gmail.com>
2015-04-11 17:09:57 +0200

    Provided documentation for the EditDistanceExtension class.

--- EditDistanceExtension.cs
+++ EditDistanceExtension.cs
@@ -4,8 +4,17 @@ using System.Linq;
 
 namespace CommandLineParsing
 {
+    /// <summary>
+    /// Provides a collection of methods for calculating the edit distance between two strings (see http://en.wikipedia.org/wiki/Edit_distance).
+    /// </summary>
     public static class EditDistanceExtension
     {
+        /// <summary>
+        /// Orders the strings in <paramref name="strings"/> according to their edit distance to <paramref name="origin"/>.
+        /// </summary>
+        /// <param name="strings">The strings to which edit distance is calculated.</param>
+        /// <param name="origin">The origin string from which edit distance is calculated.</param>
+        /// <returns>An ordered collection of strings and their edit distance to <paramref name="origin"/>, ordered by the edit distance (ascending).</returns>
         public static IEnumerable<Tuple<string, int>> OrderByDistance(this IEnumerable<string> strings, string origin)
         {
             Tuple<string, int>[] dist = strings.Select(x => Tuple.Create(x, EditDistanceTo(origin, x))).ToArray();
@@ -15,6 +24,12 @@ namespace CommandLineParsing
                 yield return t;
         }
 
+        /// <summary>
+        /// Calculates the edit distance from <paramref name="origin"/> to <paramref name="str"/>.
+        /// </summary>
+        /// <param name="origin">The origin string from which edit distance is calculated.</param>
+        /// <param name="str">The target string to which edit distance is calculated.</param>
+        /// <returns>The edit distance between the two strings (see http://en.wikipedia.org/wiki/Edit_distance). </returns>
         public static int EditDistanceTo(this string origin, string str)
         {
             int[,] dist = new int[origin.Length + 1, str.Length + 1];
