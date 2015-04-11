fbad0ccd143b7d41e2a707d849250d39b47fae90
deaddog <mikkelslsl@gmail.com>
2015-04-11 22:48:29 +0200

    Copied the MenuLabeling enum from the DeadDog repo.

---
+++ MenuLabeling.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParsing
+{
+    /// <summary>
+    /// Defines the labels used when displaying menues.
+    /// </summary>
+    public enum MenuLabeling
+    {
+        /// <summary>
+        /// No labeling is used.
+        /// </summary>
+        None,
+        /// <summary>
+        /// Numbers (0-9) are used.
+        /// </summary>
+        Numbers,
+        /// <summary>
+        /// Letters (a-z) are used.
+        /// </summary>
+        Letters,
+        /// <summary>
+        /// Numbers (0-9) and then letters (a-z) are used.
+        /// </summary>
+        NumbersAndLetters
+    }
+}
