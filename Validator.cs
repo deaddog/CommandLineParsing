a5c2a31f901e617014c97eb4a92265a6df9dcb73
deaddog <mikkelslsl@gmail.com>
2015-04-14 19:49:50 +0200

    Created a non-generic version of the Validator class.

--- Validator.cs
+++ Validator.cs
@@ -7,27 +7,26 @@ using System.Threading.Tasks;
 namespace CommandLineParsing
 {
     /// <summary>
-    /// Defines a collection of validation methods, that can be applied to data of the specified type <typeparamref name="T"/>.
+    /// Defines a collection of validation methods.
     /// </summary>
-    /// <typeparam name="T">The type of elements validated by the <see cref="Validator{T}"/>.</typeparam>
-    public class Validator<T>
+    public class Validator
     {
-        private List<Func<T, Message>> validators;
+        private List<Func<Message>> validators;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Validator{T}"/> class that accepts any <typeparamref name="T"/> values.
+        /// Initializes a new instance of the <see cref="Validator"/> class.
         /// </summary>
         public Validator()
         {
-            this.validators = new List<Func<T, Message>>();
+            this.validators = new List<Func<Message>>();
         }
 
         /// <summary>
-        /// Provides a validation method for this <see cref="Validator{T}"/>.
+        /// Provides a validation method for this <see cref="Validator"/>.
         /// </summary>
-        /// <param name="validator">A function that validates the parsed <typeparamref name="T"/> value and returns a <see cref="Message"/>.
+        /// <param name="validator">A function that validates state and returns a <see cref="Message"/>.
         /// If the validation was successful <see cref="Message.NoError"/> should be returned by the method; otherwise an appropriate <see cref="Message"/> should be returned.</param>
-        public void Add(Func<T, Message> validator)
+        public void Add(Func<Message> validator)
         {
             if (validator == null)
                 throw new ArgumentNullException("validator");
@@ -36,15 +35,40 @@ namespace CommandLineParsing
         }
 
         /// <summary>
-        /// Validates <paramref name="value"/> using the validation methods stored in this <see cref="Validator{T}"/>.
+        /// Validates that only one of <paramref name="parameters"/> is set.
         /// </summary>
-        /// <param name="value">The value to validate.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns><see cref="Message.NoError"/> if zero or one <see cref="Parameter"/> is set; otherwise an error message describing the problem.</returns>
+        public void AddOnlyOne(params Parameter[] parameters)
+        {
+            Add(() =>
+            {
+                Parameter first = null;
+
+                for (int i = 0; i < parameters.Length; i++)
+                    if (parameters[i].IsSet)
+                    {
+                        if (first == null)
+                            first = parameters[i];
+                        else
+                            return string.Format("The {0} {1} cannot be used with the {2} {3}.",
+                                first.Name, first is FlagParameter ? "flag" : "parameter",
+                                parameters[i].Name, parameters[i] is FlagParameter ? "flag" : "parameter");
+                    }
+
+                return Message.NoError;
+            });
+        }
+
+        /// <summary>
+        /// Validates using the validation methods stored in this <see cref="Validator"/>.
+        /// </summary>
         /// <returns>A <see cref="Message"/> representing the error that occured during validation; or <see cref="Message.NoError"/> if no error occured.</returns>
-        public Message Validate(T value)
+        public Message Validate()
         {
             for (int i = 0; i < validators.Count; i++)
             {
-                var msg = validators[i](value);
+                var msg = validators[i]();
                 if (msg.IsError)
                     return msg;
             }
---
+++ Validator[T].cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParsing
+{
+    /// <summary>
+    /// Defines a collection of validation methods, that can be applied to data of the specified type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of elements validated by the <see cref="Validator{T}"/>.</typeparam>
+    public class Validator<T>
+    {
+        private List<Func<T, Message>> validators;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Validator{T}"/> class that accepts any <typeparamref name="T"/> values.
+        /// </summary>
+        public Validator()
+        {
+            this.validators = new List<Func<T, Message>>();
+        }
+
+        /// <summary>
+        /// Provides a validation method for this <see cref="Validator{T}"/>.
+        /// </summary>
+        /// <param name="validator">A function that validates the parsed <typeparamref name="T"/> value and returns a <see cref="Message"/>.
+        /// If the validation was successful <see cref="Message.NoError"/> should be returned by the method; otherwise an appropriate <see cref="Message"/> should be returned.</param>
+        public void Add(Func<T, Message> validator)
+        {
+            if (validator == null)
+                throw new ArgumentNullException("validator");
+
+            this.validators.Add(validator);
+        }
+
+        /// <summary>
+        /// Validates <paramref name="value"/> using the validation methods stored in this <see cref="Validator{T}"/>.
+        /// </summary>
+        /// <param name="value">The value to validate.</param>
+        /// <returns>A <see cref="Message"/> representing the error that occured during validation; or <see cref="Message.NoError"/> if no error occured.</returns>
+        public Message Validate(T value)
+        {
+            for (int i = 0; i < validators.Count; i++)
+            {
+                var msg = validators[i](value);
+                if (msg.IsError)
+                    return msg;
+            }
+
+            return Message.NoError;
+        }
+    }
+}
