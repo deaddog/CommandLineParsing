09810e0f041afcfdc8914061e5a986fab1482e30
deaddog <mikkelslsl@gmail.com>
2015-05-01 20:24:46 +0200

    Created a set of MenuSelect methods that provide a simple interface for selecting an element in a collection (IEnumerable).

--- MenuExtension.cs
+++ MenuExtension.cs
@@ -51,6 +51,49 @@ namespace CommandLineParsing
         }
 
         /// <summary>
+        /// Displays a <see cref="Menu{T}"/> where an element from the collection can be selected.
+        /// </summary>
+        /// <typeparam name="T">The type of element in <typeparamref name="collection"/>.</typeparam>
+        /// <param name="collection">The collection of element from which the <see cref="Menu{T}"/> should be created.</param>
+        /// <param name="settings">A <see cref="MenuSettings"/> that expresses the settings used when displaying the menu, or <c>null</c> to use the default settings.</param>
+        /// <returns>The element that was selected using the displayed <see cref="Menu{T}"/>.</returns>
+        public static T MenuSelect<T>(this IEnumerable<T> collection, MenuSettings settings)
+        {
+            return MenuSelect(collection, settings, x => x.ToString());
+        }
+        /// <summary>
+        /// Displays a <see cref="Menu{T}"/> where an element from the collection can be selected.
+        /// </summary>
+        /// <typeparam name="T">The type of element in <typeparamref name="collection"/>.</typeparam>
+        /// <param name="collection">The collection of element from which the <see cref="Menu{T}"/> should be created.</param>
+        /// <param name="settings">A <see cref="MenuSettings"/> that expresses the settings used when displaying the menu, or <c>null</c> to use the default settings.</param>
+        /// <param name="keySelector">A function that gets the <see cref="String"/> that should be displayed for an item in the collection</param>
+        /// <returns>The element that was selected using the displayed <see cref="Menu{T}"/>.</returns>
+        public static T MenuSelect<T>(this IEnumerable<T> collection, MenuSettings settings, Func<T, string> keySelector)
+        {
+            Menu<T> menu = new Menu<T>();
+
+            foreach (var item in collection)
+                menu.Add(keySelector(item), item);
+
+            return menu.ShowAndSelect(settings).Value;
+        }
+
+        /// <summary>
+        /// Displays a <see cref="Menu{T}"/> where an element from the collection can be selected.
+        /// Displays the key part of each element in the menu and returns the selected value part.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the Key part of elements in <paramref name="collection"/>.</typeparam>
+        /// <typeparam name="TValue">The type of the Value part of elements in <paramref name="collection"/>.</typeparam>
+        /// <param name="collection">The collection of element from which the <see cref="Menu{T}"/> should be created.</param>
+        /// <param name="settings">A <see cref="MenuSettings"/> that expresses the settings used when displaying the menu, or <c>null</c> to use the default settings.</param>
+        /// <returns>The element that was selected using the displayed <see cref="Menu{T}"/>.</returns>
+        public static TValue MenuSelect<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> collection, MenuSettings settings)
+        {
+            return MenuSelect(collection, settings, x => x.Key.ToString()).Value;
+        }
+
+        /// <summary>
         /// Adds a new option to the menu, which returns a constant value.
         /// </summary>
         /// <param name="menu">The menu to which the option is added.</param>
