c4cb3e214bd9e821480620067a7bf20ef0c06017
deaddog <mikkelslsl@gmail.com>
2015-07-21 23:58:27 +0200

    Removed the Action/Func specific menu extension methods from MenuExtension.

--- MenuExtension.cs
+++ MenuExtension.cs
@@ -9,48 +9,6 @@ namespace CommandLineParsing
     public static class MenuExtension
     {
         /// <summary>
-        /// Shows the menu and waits for an option to be selected.
-        /// When an option has been selected, its corresponding delegate is executed.
-        /// If <paramref name="repeat"/> is <c>true</c> this process is repeated until the cancel option is selected.
-        /// </summary>
-        /// <param name="menu">The menu that should be shown.</param>
-        /// <param name="settings">A <see cref="MenuSettings"/> that expresses the settings used when displaying the menu, or <c>null</c> to use the default settings.</param>
-        /// <param name="repeat">A boolean indicating whether the menu should be displayed repeatedly until the cancel option is selected.</param>
-        public static void Show(this Menu<Action> menu, MenuSettings settings, bool repeat = false)
-        {
-            if (!menu.CanCancel && repeat)
-                throw new InvalidOperationException("A menu cannot auto-repeat without a cancel option.");
-
-            foreach (var s in menu.ShowAndSelectRange(settings, false))
-                s.Value();
-        }
-        /// <summary>
-        /// Shows the menu and waits for an option to be selected.
-        /// When an option has been selected, its corresponding delegate is executed.
-        /// </summary>
-        /// <param name="menu">The menu that should be shown.</param>
-        /// <param name="settings">A <see cref="MenuSettings"/> that expresses the settings used when displaying the menu, or <c>null</c> to use the default settings.</param>
-        /// <returns>
-        /// The value returned by the delegate called (dependant on the option selected).
-        /// </returns>
-        public static T Show<T>(this Menu<Func<T>> menu, MenuSettings settings)
-        {
-            return menu.ShowAndSelect(settings).Value();
-        }
-        /// <summary>
-        /// Shows the menu and waits for an option to be selected.
-        /// When an option has been selected, its corresponding delegate is executed.
-        /// </summary>
-        /// <param name="menu">The menu that should be shown.</param>
-        /// <param name="settings">A <see cref="MenuSettings"/> that expresses the settings used when displaying the menu, or <c>null</c> to use the default settings.</param>
-        /// <returns>An <see cref="IEnumerable{T}"/> that contains the selected elements (one for each time the menu is displayed).</returns>
-        public static IEnumerable<T> ShowRange<T>(this Menu<Func<T>> menu, MenuSettings settings)
-        {
-            foreach (var s in menu.ShowAndSelectRange(settings, false))
-                yield return s.Value();
-        }
-
-        /// <summary>
         /// Displays a <see cref="Menu{T}"/> where an element from the collection can be selected.
         /// </summary>
         /// <typeparam name="T">The type of element in <typeparamref name="collection"/>.</typeparam>
