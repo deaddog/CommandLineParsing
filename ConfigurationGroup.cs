dbeb1ab36420bc1527c09830bb4e17fc980852f4
deaddog <mikkelslsl@gmail.com>
2015-07-30 22:42:05 +0200

    Created the members defined by IConfiguration in GroupConfiguration as NotImplementedException implementations.

--- ConfigurationGroup.cs
+++ ConfigurationGroup.cs
@@ -41,5 +41,40 @@ namespace CommandLineParsing
 
             this.configurations = new LinkedList<IConfiguration>(configurations);
         }
+
+        /// <summary>
+        /// Gets the <see cref="string"/> value with the specified key.
+        /// </summary>
+        /// <param name="key">The key (name.name) that the value is associated with.</param>
+        /// <returns>The <see cref="string"/> value that corresponds to <paramref name="key"/> from the first configuration where <paramref name="key"/> is defined.</returns>
+        public string this[string key]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the specified key exists in the configuration.
+        /// </summary>
+        /// <param name="key">The key that is looked for in the configuration.</param>
+        /// <returns><c>true</c>, if <paramref name="key"/> is defined any of the configurations in this <see cref="ConfigurationGroup"/>; otherwise, <c>false</c>.</returns>
+        public bool HasKey(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets all the key/value pairs in the configuration.
+        /// If a key is defined in multiple configurations, only the first one found is returned.
+        /// </summary>
+        /// <returns>
+        /// A collection of all key/value pairs in the configuration.
+        /// </returns>
+        public IEnumerable<KeyValuePair<string, string>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
