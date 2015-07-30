64b7e4451c658cb45f00e3768bcf69fb9a0c4fa3
deaddog <mikkelslsl@gmail.com>
2015-07-30 22:25:04 +0200

    Created an empty class for implementing configuration fallback (grouping) for #8.

---
+++ ConfigurationGroup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommandLineParsing
+{
+    /// <summary>
+    /// Provides acces to multiple <see cref="IConfiguration"/> configurations through one element.
+    /// </summary>
+    public class ConfigurationGroup : IConfiguration
+    {
+    }
+}
