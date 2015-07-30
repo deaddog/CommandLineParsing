2efc9bef244ee72ff00e15ca3748f18f28030b3e
deaddog <mikkelslsl@gmail.com>
2015-07-30 22:41:11 +0200

    Added constructors to the ConfigurationGroup constructor.

--- ConfigurationGroup.cs
+++ ConfigurationGroup.cs
@@ -8,5 +8,38 @@ namespace CommandLineParsing
     /// </summary>
     public class ConfigurationGroup : IConfiguration
     {
+        private LinkedList<IConfiguration> configurations;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigurationGroup"/> class.
+        /// </summary>
+        public ConfigurationGroup()
+        {
+            this.configurations = new LinkedList<IConfiguration>();
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigurationGroup"/> class.
+        /// </summary>
+        /// <param name="configurations">The configurations that are copied to the new <see cref="ConfigurationGroup"/>.
+        /// Values in the first element will override any in the latter ones when they have equal keys.</param>
+        public ConfigurationGroup(params IConfiguration[] configurations)
+        {
+            if (configurations == null)
+                throw new ArgumentNullException(nameof(configurations));
+
+            this.configurations = new LinkedList<IConfiguration>(configurations);
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigurationGroup"/> class.
+        /// </summary>
+        /// <param name="configurations">The configurations that are copied to the new <see cref="ConfigurationGroup"/>.
+        /// Values in the first element will override any in the latter ones when they have equal keys.</param>
+        public ConfigurationGroup(IEnumerable<IConfiguration> configurations)
+        {
+            if (configurations == null)
+                throw new ArgumentNullException(nameof(configurations));
+
+            this.configurations = new LinkedList<IConfiguration>(configurations);
+        }
     }
 }
