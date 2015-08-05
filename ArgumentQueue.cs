7792733dd130a39ce5bc5bbc3b5d90986a80ceb9
deaddog <mikkelslsl@gmail.com>
2015-08-05 16:07:00 +0200

    Created a class 'ArgumentQueue' that will replace the queue management in the executor.

---
+++ ArgumentQueue.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CommandLineParsing
+{
+    internal class ArgumentQueue
+    {
+        private int index;
+        private List<string> args;
+        private List<string> accepted;
+        
+        public ArgumentQueue(IEnumerable<string> args)
+        {
+            this.index = 0;
+            this.args = new List<string>(args);
+            this.accepted = new List<string>();
+        }
+
+        public int Count
+        {
+            get { return args.Count - index; }
+        }
+
+        public string Peek
+        {
+            get { return index < args.Count ? args[index] : null; }
+        }
+        public string Dequeue()
+        {
+            if (index >= args.Count)
+                throw new InvalidOperationException($"Nothing can be taken as the of the {nameof(ArgumentQueue)} is empty.");
+
+            string take = args[index];
+            args.RemoveAt(index);
+            return take;
+        }
+
+        public void Accept()
+        {
+            if (index >= args.Count)
+                throw new InvalidOperationException($"Nothing can be accepted as the of the {nameof(ArgumentQueue)} is empty.");
+
+            accepted.Add(args[index]);
+        }
+        public void Skip()
+        {
+            if (index >= args.Count)
+                throw new InvalidOperationException($"Nothing can be skipped as the of the {nameof(ArgumentQueue)} is empty.");
+
+            index++;
+        }
+
+        public string[] PopAccepted()
+        {
+            string[] pop = accepted.ToArray();
+            accepted.Clear();
+            return pop;
+        }
+        public string[] PopSkipped()
+        {
+            var pop = args.Take(index).ToArray();
+            index = 0;
+            return pop;
+        }
+    }
+}
