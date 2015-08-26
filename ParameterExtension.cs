56b8cf5c171b994958449ceca82e8fa8cf6cdf8c
deaddog <mikkelslsl@gmail.com>
2015-08-26 14:23:39 +0200

    Parameter parser can be set using an extension method.

---
+++ ParameterExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CommandLineParsing
+{
+    /// <summary>
+    /// Provides additional methods for the <see cref="Parameter"/> class.
+    /// </summary>
+    public static class ParameterExtension
+    {
+        /// <summary>
+        /// Sets the parser used by the <see cref="Parameter{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the value managed by the parameter.</typeparam>
+        /// <param name="parameter">The parameter.</param>
+        /// <param name="parser">The new parser.</param>
+        public static void SetParser<T>(this Parameter<T> parameter, TryParse<T> parser)
+        {
+            var type = parameter.GetType();
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(ArrayParameter<>))
+                throw new InvalidOperationException($"This method cannot be used to set parser for {nameof(ArrayParameter<T>)}.");
+            else
+                parameter.setParser(parser);
+        }
+
+        /// <summary>
+        /// Sets the parser used by the <see cref="Parameter{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the values managed by the parameter.</typeparam>
+        /// <param name="parameter">The parameter.</param>
+        /// <param name="parser">The new parser.</param>
+        public static void SetParser<T>(this Parameter<T[]> parameter, TryParse<T> parser)
+        {
+            (parameter as ArrayParameter<T>)?.setParser(parser);
+        }
+    }
+}
