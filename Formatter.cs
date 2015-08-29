44256a19137b235dd003b5c0c0e2291b2099c277
deaddog <mikkelslsl@gmail.com>
2015-08-29 20:56:20 +0200

    Created a new class Formatter which implements the IFormatter interface.

---
+++ Formatter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CommandLineParsing
+{
+    public class Formatter : IFormatter
+    {
+        public string GetVariable(string variable)
+        {
+            throw new NotImplementedException();
+        }
+        public string GetAutoColor(string variable)
+        {
+            throw new NotImplementedException();
+        }
+        public int? GetAlignedLength(string variable)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool? ValidateCondition(string condition)
+        {
+            throw new NotImplementedException();
+        }
+        public string EvaluateFunction(string function, string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
