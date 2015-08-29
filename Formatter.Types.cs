7b5b62e93a5506b651e979b692078465f39a3189
deaddog <mikkelslsl@gmail.com>
2015-08-29 21:19:17 +0200

    Created a base type for the collections in Formatter and a base type for their elements.

--- Formatter.Types.cs
+++ Formatter.Types.cs
@@ -1,15 +1,45 @@
-﻿namespace CommandLineParsing
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommandLineParsing
 {
     public partial class Formatter
     {
+        public class Item
+        {
+            public readonly Type Type;
+
+            internal Item(Type type)
+            {
+                if (type == null)
+                    throw new ArgumentNullException(nameof(type));
+
+                this.Type = type;
+            }
+        }
+        public class Collection<T> where T : Item
+        {
+            private Dictionary<string, T> elements;
+
+            internal Collection()
+            {
+                this.elements = new Dictionary<string, T>();
+            }
+
+            internal bool TryGet(string identifier, out T item)
+            {
+                return elements.TryGetValue(identifier, out item);
+            }
+        }
+        
         public class VariableCollection
         {
         }
-
+        
         public class ConditionCollection
         {
         }
-
+        
         public class FunctionCollection
         {
         }
