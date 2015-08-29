7b7db3ba322290003b27e470f1150d2b31319214
deaddog <mikkelslsl@gmail.com>
2015-08-29 21:50:06 +0200

    Implemented a generic EvaluateFormat method in the Formatter class.

--- Formatter.cs
+++ Formatter.cs
@@ -1,9 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CommandLineParsing
 {
     public class Formatter : IFormatter
     {
+        private Dictionary<Type, object> items;
+
+        public Formatter()
+        {
+            this.items = new Dictionary<Type, object>();
+        }
+
+        public string EvaluateFormat<T>(T item, string format)
+        {
+            var type = typeof(T);
+            bool hasOld = items.ContainsKey(type);
+            object old = hasOld ? items[type] : null;
+
+            items[type] = item;
+            string res = ColorConsole.EvaluateFormat(format, this);
+
+            if (hasOld)
+                items[type] = old;
+            else
+                items.Remove(type);
+
+            return res;
+        }
+
         public string GetVariable(string variable)
         {
             throw new NotImplementedException();
