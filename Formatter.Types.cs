65abd1a0d37879a213a2fc19e17bcb4f72087512
deaddog <mikkelslsl@gmail.com>
2015-08-29 21:00:54 +0200

    Created a set of collection types nested in the Formatter class.

---
+++ Formatter.Types.cs
@@ -0,0 +1,17 @@
+﻿namespace CommandLineParsing
+{
+    public partial class Formatter
+    {
+        public class VariableCollection
+        {
+        }
+
+        public class ConditionCollection
+        {
+        }
+
+        public class FunctionCollection
+        {
+        }
+    }
+}
--- Formatter.cs
+++ Formatter.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 
 namespace CommandLineParsing
 {
-    public class Formatter : IFormatter
+    public partial class Formatter : IFormatter
     {
+        private VariableCollection variables;
+        private ConditionCollection conditions;
+        private FunctionCollection functions;
+
         private Dictionary<Type, object> items;
 
         public Formatter()
         {
+            this.variables = new VariableCollection();
+            this.conditions = new ConditionCollection();
+            this.functions = new FunctionCollection();
+
             this.items = new Dictionary<Type, object>();
         }
+
+        public VariableCollection Variables => variables;
+        public ConditionCollection Conditions => conditions;
+        public FunctionCollection Functions => functions;
 
         public string EvaluateFormat<T>(T item, string format)
         {
