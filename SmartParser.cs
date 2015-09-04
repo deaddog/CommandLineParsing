adf30b476d8ee9af77f1fb8b18644652b781a3c5
deaddog <mikkelslsl@gmail.com>
2015-09-04 21:05:11 +0200

    Copied the Handle method from Parameter<T> and ArrayParameter<T> to the SmartParser.

--- SmartParser.cs
+++ SmartParser.cs
@@ -2,5 +2,52 @@
 {
     internal class SmartParser<T>
     {
+        internal override Message HandleSingle(string[] values)
+        {
+            if (parser == null)
+                parser = ParserLookup.Table.GetParser<T>(enumIgnore);
+
+            T temp;
+
+            if (values.Length == 0)
+                return noValueMessage;
+            else if (values.Length > 1)
+                return multipleValuesMessage;
+            else if (!parser(values[0], out temp))
+                return typeErrorMessage(values[0]);
+
+            var msg = validator.Validate(temp);
+            if (msg.IsError)
+                return msg;
+
+            IsSet = true;
+            value = temp;
+            doCallback();
+
+            return Message.NoError;
+        }
+        internal override Message HandleArray(string[] values)
+        {
+            if (parser == null)
+                parser = ParserLookup.Table.GetParser<T>(enumIgnore);
+
+            T[] temp = new T[values.Length];
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (!parser(values[i], out temp[i]))
+                    return TypeErrorMessage(values[i]);
+            }
+
+            var msg = validator.Validate(temp);
+            if (msg.IsError)
+                return msg;
+
+            IsSet = true;
+            value = temp;
+            doCallback();
+
+            return Message.NoError;
+        }
     }
 }
