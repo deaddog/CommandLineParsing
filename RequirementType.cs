4b7efeb5f64a6a7e72e6de366cb1b07c88a74a31
deaddog <mikkelslsl@gmail.com>
2015-12-09 09:24:21 +0100

    Created a RequirementType enum that expresses various versions of the Require attribute.

---
+++ RequirementType.cs
@@ -0,0 +1,21 @@
+﻿namespace CommandLineParsing
+{
+    /// <summary>
+    /// Describes the different types of parameter requirements for parameters with the <see cref="Required"/> attribute.
+    /// </summary>
+    public enum RequirementType
+    {
+        /// <summary>
+        /// If a value is not provided the parameter errors.
+        /// </summary>
+        Error,
+        /// <summary>
+        /// If a value is not provided the user is prompted for a value.
+        /// </summary>
+        Prompt,
+        /// <summary>
+        /// If a value is not provided the user is prompted for a value, but only if and when the <see cref="Parameter{T}.Value"/> property is used.
+        /// </summary>
+        PromptWhenUsed
+    }
+}
