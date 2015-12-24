925b2786806d6a12388968d586119da6dad77cee
deaddog <mikkelslsl@gmail.com>
2015-12-24 12:31:25 +0100

    Created structs for representing console positions/sizes.

---
+++ ConsolePoint.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CommandLineParsing
+{
+    /// <summary>
+    /// Represents a point (left/column, top/row) in the <see cref="Console"/>.
+    /// This type can be used with the <see cref="ColorConsole"/>.
+    /// </summary>
+    public struct ConsolePoint
+    {
+        private int left;
+        private int top;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConsolePoint"/> struct.
+        /// </summary>
+        /// <param name="left">The column in the console.</param>
+        /// <param name="top">The row in the console.</param>
+        public ConsolePoint(int left, int top)
+        {
+            this.left = left;
+            this.top = top;
+        }
+
+        /// <summary>
+        /// Gets or sets the column position of this <see cref="ConsolePoint"/>.
+        /// </summary>
+        public int Left
+        {
+            get { return left; }
+            set { left = value; }
+        }
+        /// <summary>
+        /// Gets or sets the row position of this <see cref="ConsolePoint"/>.
+        /// </summary>
+        public int Top
+        {
+            get { return top; }
+            set { top = value; }
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this <see cref="ConsolePoint"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString() => $"{{Left: {left}, Top: {top}}}";
+    }
+}
---
+++ ConsoleSize.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CommandLineParsing
+{
+    /// <summary>
+    /// Represents a size (width, height) in the <see cref="Console"/>.
+    /// This type can be used with the <see cref="ColorConsole"/>.
+    /// </summary>
+    public struct ConsoleSize
+    {
+        private int width;
+        private int height;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConsoleSize"/> struct.
+        /// </summary>
+        /// <param name="width">The width.</param>
+        /// <param name="height">The height.</param>
+        public ConsoleSize(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        /// <summary>
+        /// Gets or sets the width of this <see cref="ConsoleSize"/>.
+        /// </summary>
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+        /// <summary>
+        /// Gets or sets the height of this <see cref="ConsoleSize"/>.
+        /// </summary>
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this <see cref="ConsolePoint"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString() => $"{{Width: {width}, Height: {height}}}";
+    }
+}
