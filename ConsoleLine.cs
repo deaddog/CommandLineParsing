521519357b1f56d88a15c7630cc79574b8afda3f
deaddog <mikkelslsl@gmail.com>
2015-12-28 23:12:40 +0100

    Created internal classes for representing cached lines and line segments.

---
+++ ConsoleLine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CommandLineParsing
+{
+    internal class ConsoleLine
+    {
+        private LinkedList<ConsoleSegment> segments;
+
+        public ConsoleLine()
+        {
+            this.segments = new LinkedList<ConsoleSegment>();
+        }
+
+        public int Length => segments.Sum(x => x.Text.Length);
+        public bool Empty => segments.Count == 0;
+
+        public void AddSegment(ConsoleSegment segment)
+        {
+            segments.AddLast(segment);
+        }
+
+        public void WriteToConsole()
+        {
+            ConsoleColor c = Console.ForegroundColor;
+            foreach (var s in segments)
+            {
+                if (s.ForegroundColor.HasValue) Console.ForegroundColor = s.ForegroundColor.Value;
+                Console.Write(s.Text);
+            }
+            if (Length < Console.WindowWidth)
+                Console.WriteLine();
+            Console.ForegroundColor = c;
+        }
+
+        public override string ToString() => string.Join("", segments);
+    }
+}
---
+++ ConsoleSegment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CommandLineParsing
+{
+    internal class ConsoleSegment
+    {
+        public readonly ConsoleColor? ForegroundColor;
+        public readonly ConsoleColor? BackgroundColor;
+        public readonly string Text;
+
+        public ConsoleSegment(string text)
+        {
+            this.Text = text;
+            this.ForegroundColor = null;
+            this.BackgroundColor = null;
+        }
+        public ConsoleSegment(string text, ConsoleColor foreground, ConsoleColor background)
+        {
+            this.Text = text;
+            this.ForegroundColor = foreground;
+            this.BackgroundColor = background;
+        }
+        private ConsoleSegment(string text, ConsoleColor? foreground, ConsoleColor? background)
+        {
+            this.Text = text;
+            this.ForegroundColor = foreground;
+            this.BackgroundColor = background;
+        }
+
+        public override string ToString() => Text;
+    }
+}
