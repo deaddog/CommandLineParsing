74dd084f3167c1d0b43a5b5623846c5305dbdeca
deaddog <mikkelslsl@gmail.com>
2015-12-28 23:07:35 +0100

    Created a class for representing console lines that are cached for paging printing (#30).

---
+++ ConsoleCache.cs
@@ -0,0 +1,9 @@
+﻿namespace CommandLineParsing
+{
+    /// <summary>
+    /// Represents a collection of text-lines that are printable in the console.
+    /// </summary>
+    public class ConsoleCache
+    {
+    }
+}
