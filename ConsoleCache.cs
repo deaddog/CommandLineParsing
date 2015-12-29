1128e7f7cdf9cd1764cf0a00e28b98ceb1d6efd9
deaddog <mikkelslsl@gmail.com>
2015-12-29 10:16:26 +0100

    Implemented the parsing of strings without control-characters.

--- ConsoleCache.cs
+++ ConsoleCache.cs
@@ -12,11 +12,17 @@ namespace CommandLineParsing
         {
             private static readonly char[] control = new char[] { '\n', '\t', '\r', '\b', '\a' };
 
+            private readonly int lineWidth;
+            private int left;
+
             private List<ConsoleLine> lines = new List<ConsoleLine>();
             private ConsoleLine current;
 
             public Builder()
             {
+                this.lineWidth = Console.BufferWidth;
+                this.left = 0;
+
                 this.lines = new List<ConsoleLine>();
                 this.current = new ConsoleLine();
             }
@@ -31,6 +37,9 @@ namespace CommandLineParsing
             {
                 if (value == null || value.Length == 0)
                     return;
+
+                ConsoleColor fore = Console.ForegroundColor;
+                ConsoleColor back = Console.BackgroundColor;
 
                 int index = 0;
                 while (index < value.Length)
@@ -53,7 +62,29 @@ namespace CommandLineParsing
                             throw new NotImplementedException();
 
                         default:
-                            throw new NotImplementedException();
+                            int nIndex = value.IndexOfAny(control, index);
+                            if (nIndex < 0) nIndex = value.Length;
+
+                            while (index < nIndex)
+                            {
+                                int len = nIndex - index;
+
+                                if (len >= lineWidth - left)
+                                {
+                                    len = lineWidth - left;
+                                    current.InsertSegment(new ConsoleSegment(value.Substring(index, len), fore, back), left);
+                                    addCurrent();
+                                    left = 0;
+                                    index += len;
+                                }
+                                else
+                                {
+                                    current.InsertSegment(new ConsoleSegment(value.Substring(index, len), fore, back), left);
+                                    left += len;
+                                    index += len;
+                                }
+                            }
+                            break;
                     }
                 }
             }
