a9aed945a7981db472ba1e48553e13d715aebd8c
deaddog <mikkelslsl@gmail.com>
2015-12-29 09:39:09 +0100

    The cache is represented as an array of lines.

--- ConsoleCache.cs
+++ ConsoleCache.cs
@@ -1,9 +1,17 @@
-﻿namespace CommandLineParsing
+﻿using System.Collections.Generic;
+
+namespace CommandLineParsing
 {
     /// <summary>
     /// Represents a collection of text-lines that are printable in the console.
     /// </summary>
     public class ConsoleCache
     {
+        private ConsoleLine[] lines;
+
+        private ConsoleCache(ConsoleLine[] lines)
+        {
+            this.lines = lines;
+        }
     }
 }
