21a61d0f7d61d229fa01cc7267de268caf58dc51
deaddog <mikkelslsl@gmail.com>
2015-12-29 09:43:33 +0100

    Created an internal class Builder, nested in ConsoleCache, that will capture any write-lines and construct a cache for printing.

--- ConsoleCache.cs
+++ ConsoleCache.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace CommandLineParsing
 {
@@ -7,6 +8,40 @@ namespace CommandLineParsing
     /// </summary>
     public class ConsoleCache
     {
+        internal class Builder
+        {
+            private List<ConsoleLine> lines = new List<ConsoleLine>();
+            private ConsoleLine current;
+
+            public Builder()
+            {
+                this.lines = new List<ConsoleLine>();
+                this.current = new ConsoleLine();
+            }
+
+            private void addCurrent()
+            {
+                lines.Add(current);
+                current = new ConsoleLine();
+            }
+
+            public void WriteString(string value)
+            {
+                throw new NotImplementedException();
+            }
+
+            public ConsoleCache ConstructCache()
+            {
+                if (!current.Empty)
+                    addCurrent();
+
+                var arr = lines.ToArray();
+                lines.Clear();
+
+                return new ConsoleCache(arr);
+            }
+        }
+
         private ConsoleLine[] lines;
 
         private ConsoleCache(ConsoleLine[] lines)
