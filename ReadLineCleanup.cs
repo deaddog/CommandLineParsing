da15b61c7feb15a8ce516854068c0ce4240fec38
deaddog <mikkelslsl@gmail.com>
2016-09-16 19:32:26 +0200

    Created enum that determines what type of cleanup should be applied after an execution of readline.

---
+++ ReadLineCleanup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParsing
+{
+    /// <summary>
+    /// Defines what type of console cleanup should be applied after completing a readline.
+    /// </summary>
+    public enum ReadLineCleanup
+    {
+        /// <summary>
+        /// No cleanup is applied.
+        /// </summary>
+        None,
+        /// <summary>
+        /// Only the prompt displayed with the readline is removed.
+        /// </summary>
+        RemovePrompt,
+        /// <summary>
+        /// Both the prompt and the entered value are removed.
+        /// </summary>
+        RemoveAll
+    }
+}
