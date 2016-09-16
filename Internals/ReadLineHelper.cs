2e15e5f4245895d9002841b10ec9bd21bce2d4fe
deaddog <mikkelslsl@gmail.com>
2016-09-16 15:54:10 +0200

    Created a helper class for readline implementation.

---
+++ Internals/ReadLineHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace CommandLineParsing.Internals
+{
+    internal class ReadLineHelper
+    {
+        private readonly int position;
+        private readonly StringBuilder sb;
+
+        public ReadLineHelper()
+        {
+            position = Console.CursorLeft;
+            sb = new StringBuilder();
+        }
+    }
+}
