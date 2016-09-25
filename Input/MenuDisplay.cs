d9c1568d725c817fba989d132151f5c0a217c978
deaddog <mikkelslsl@gmail.com>
2016-09-25 10:17:06 +0200

    Defined an empty type for displaying a menu and a basic type for handling menu options.

---
+++ Input/MenuDisplay.cs
@@ -0,0 +1,36 @@
+﻿namespace CommandLineParsing.Input
+{
+    /// <summary>
+    /// Provides methods for managing a menu in the console.
+    /// </summary>
+    public class MenuDisplay
+    {
+        private readonly ConsolePoint origin;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MenuDisplay"/> class.
+        /// The menu will be displayed at the current cursor position.
+        /// </summary>
+        public MenuDisplay()
+            : this(ColorConsole.CursorPosition)
+        {
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MenuDisplay"/> class.
+        /// </summary>
+        /// <param name="point">The point where the menu should be displayed.</param>
+        public MenuDisplay(ConsolePoint point)
+        {
+            origin = point;
+            options = new List<MenuOption<T>>();
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MenuDisplay{T}"/> class.
+        /// </summary>
+        /// <param name="offset">The offset from the current cursor position where to menu should be displayed.</param>
+        public MenuDisplay(ConsoleSize offset)
+            : this(ColorConsole.CursorPosition + offset)
+        {
+        }
+    }
+}
---
+++ Input/MenuOption.cs
@@ -0,0 +1,26 @@
+﻿namespace CommandLineParsing.Input
+{
+    /// <summary>
+    /// Represemts a single option in a menu.
+    /// </summary>
+    public abstract class MenuOption<T>
+    {
+        /// <summary>
+        /// The text displayed in the menu for this option.
+        /// </summary>
+        public abstract string Text { get; }
+        /// <summary>
+        /// The value associated with this menu option.
+        /// </summary>
+        public T Value { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MenuOption{T}"/> class.
+        /// </summary>
+        /// <param name="value">The value assocated with the option.</param>
+        public MenuOption(T value)
+        {
+            Value = value;
+        }
+    }
+}
