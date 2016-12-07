e166f51ba2fa19840d6695f8534f193914d8a551
deaddog <mikkelslsl@gmail.com>
2016-12-07 21:59:57 +0100

    A ConsoleString can be constructed from a formatted string.

--- Output/ConsoleString.cs
+++ Output/ConsoleString.cs
@@ -1,4 +1,6 @@
-﻿namespace CommandLineParsing.Output
+﻿using System.Linq;
+
+namespace CommandLineParsing.Output
 {
     /// <summary>
     /// Represents a string with embedded color-information.
@@ -18,6 +20,16 @@
         public ConsoleString()
         {
             content = new Segment[0];
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConsoleString"/> class from a string.
+        /// Any color-encoding will be parsed by the constructor.
+        /// </summary>
+        /// <param name="content">The content that should be contained by the <see cref="ConsoleString"/>.
+        /// The string "[Color:Text]" will translate to a <see cref="ConsoleString"/> using Color as the foreground color.</param>
+        public ConsoleString(string content)
+        {
+            this.content = Segment.Parse(content, true).ToArray();
         }
     }
 }
