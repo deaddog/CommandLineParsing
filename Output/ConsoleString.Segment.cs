d2c90171200d3ee047bc4f270205b65e6ceb6e1e
deaddog <mikkelslsl@gmail.com>
2016-12-07 19:40:34 +0100

    Created classes for representing colored strings.

---
+++ Output/ConsoleString.Segment.cs
@@ -0,0 +1,17 @@
+﻿namespace CommandLineParsing.Output
+{
+    public partial class ConsoleString
+    {
+        private class Segment
+        {
+            public readonly string Content;
+            public readonly string Color;
+
+            public Segment(string content, string color)
+            {
+                Content = content;
+                Color = color;
+            }
+        }
+    }
+}
---
+++ Output/ConsoleString.cs
@@ -0,0 +1,23 @@
+﻿namespace CommandLineParsing.Output
+{
+    /// <summary>
+    /// Represents a string with embedded color-information.
+    /// </summary>
+    public partial class ConsoleString
+    {
+        private readonly Segment[] content;
+
+        /// <summary>
+        /// Gets a <see cref="ConsoleString"/> that represents the empty string.
+        /// </summary>
+        public static ConsoleString Empty => new ConsoleString();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConsoleString"/> class, representing an empty string.
+        /// </summary>
+        public ConsoleString()
+        {
+            content = new Segment[0];
+        }
+    }
+}
