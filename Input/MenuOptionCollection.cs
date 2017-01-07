f5bb087278cb2b7ffbffc97ba6a1b5834816b500
deaddog <mikkelslsl@gmail.com>
2017-01-07 17:39:57 +0100

    The MenuOptionCollection is defined as an implementation of IList<>.

--- Input/MenuOptionCollection.cs
+++ Input/MenuOptionCollection.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace CommandLineParsing.Input
 {
@@ -6,7 +8,7 @@ namespace CommandLineParsing.Input
     /// Defines a collection of <see cref="MenuOption{T}"/> that is displayed by a <see cref="MenuDisplay{T}"/>.
     /// </summary>
     /// <typeparam name="T">The type of the values managed by the <see cref="MenuOptionCollection{T}"/>.</typeparam>
-    public class MenuOptionCollection<T>
+    public class MenuOptionCollection<T> : IList<MenuOption<T>>
     {
         private readonly MenuDisplay<T> _display;
         private readonly List<MenuOption<T>> _options;
@@ -23,11 +25,18 @@ namespace CommandLineParsing.Input
         public int Count => _options.Count;
 
         /// <summary>
-        /// Gets the <see cref="MenuOption{T}"/> at the specified index.
+        /// Gets or sets the <see cref="MenuOption{T}"/> at the specified index.
         /// </summary>
-        /// <param name="index">The index.</param>
+        /// <param name="index">The option index.</param>
         /// <returns>The <see cref="MenuOption{T}"/> at the specified index.</returns>
-        public MenuOption<T> this[int index] => _options[index];
+        public MenuOption<T> this[int index]
+        {
+            get { return _options[index]; }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
 
         /// <summary>
         /// Gets the index of a specific <see cref="MenuOption{T}"/> in the collection.
@@ -37,6 +46,51 @@ namespace CommandLineParsing.Input
         public int IndexOf(MenuOption<T> option)
         {
             return _options.IndexOf(option);
+        }
+
+        public bool Contains(MenuOption<T> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(MenuOption<T> item)
+        {
+            throw new NotImplementedException();
+        }
+        public void Insert(int index, MenuOption<T> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(MenuOption<T> item)
+        {
+            throw new NotImplementedException();
+        }
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool ICollection<MenuOption<T>>.IsReadOnly => false;
+        void ICollection<MenuOption<T>>.CopyTo(MenuOption<T>[] array, int arrayIndex)
+        {
+            _options.CopyTo(array, arrayIndex);
+        }
+
+        IEnumerator<MenuOption<T>> IEnumerable<MenuOption<T>>.GetEnumerator()
+        {
+            foreach (var o in _options)
+                yield return o;
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            foreach (var o in _options)
+                yield return o;
         }
     }
 }
