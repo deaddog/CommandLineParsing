dec981045ec93be276594cdeb0f7412fdd3edcec
deaddog <mikkelslsl@gmail.com>
2017-01-10 09:21:45 +0100

    Extended the console input interface with cleanup details.

--- Input/IConsoleInput.cs
+++ Input/IConsoleInput.cs
@@ -5,12 +5,17 @@ namespace CommandLineParsing.Input
     /// <summary>
     /// Provides a method for handling keyboard input in console applications.
     /// </summary>
-    public interface IConsoleInput
+    public interface IConsoleInput : IDisposable
     {
         /// <summary>
         /// Handles the specified key by updating internal/visual state.
         /// </summary>
         /// <param name="key">The key to process.</param>
         void HandleKey(ConsoleKeyInfo key);
+
+        /// <summary>
+        /// Gets the type of cleanup that should be applied when disposing the <see cref="IConsoleInput"/>.
+        /// </summary>
+        InputCleanup Cleanup { get; }
     }
 }
