fac1c8789a9030b373a9d81fbe245ac46f4fd556
deaddog <mikkelslsl@gmail.com>
2017-01-10 09:04:28 +0100

    Defined a new cleanup enum, generic to multiple input types.

---
+++ Input/InputCleanup.cs
@@ -0,0 +1,17 @@
+﻿namespace CommandLineParsing.Input
+{
+    /// <summary>
+    /// Defines what type of console cleanup should be applied after retrieving console input.
+    /// </summary>
+    public enum InputCleanup
+    {
+        /// <summary>
+        /// No cleanup is applied.
+        /// </summary>
+        None,
+        /// <summary>
+        /// Removes the input from the console.
+        /// </summary>
+        Clean,
+    }
+}
