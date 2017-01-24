0379aba861188644ff28a476a51357aac9c8790a
deaddog <mikkelslsl@gmail.com>
2017-01-24 08:22:48 +0100

    Created new MenuSelect extension methods that unifies the functionality of the previous MenuSelect extensions.

--- MenuExtension.cs
+++ MenuExtension.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CommandLineParsing.Input;
+using CommandLineParsing.Output;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CommandLineParsing
 {
@@ -70,6 +73,108 @@ namespace CommandLineParsing
                 menu.SetCancel(cancelKey, cancelValue);
 
             return menu.ShowAndSelect(settings).Value;
+        }
+
+        /// <summary>
+        /// Displays a menu where a single element can be selected from the collection.
+        /// </summary>
+        /// <typeparam name="T">The type of element in <paramref name="collection"/>.</typeparam>
+        /// <param name="collection">The collection of element from which the menu should be created.</param>
+        /// <param name="keySelector">A function that gets the <see cref="string"/> that should be displayed for an item in the collection</param>
+        /// <param name="labeling">The type of labeling (option prefix) that should be applied when displaying the menu.</param>
+        /// <param name="cleanup">The cleanup applied after displaying the menu.</param>
+        /// <param name="cancelKey">If not <c>null</c>, this string is displayed as a "cancel option" in the bottom of the menu.
+        /// <paramref name="cancelValue"/> will be returned if this option is selected.</param>
+        /// <param name="cancelValue">The value returned if <paramref name="cancelKey"/> is not <c>null</c>.</param>
+        /// <returns>The element that was selected using the displayed menu.</returns>
+        public static T MenuSelect<T>(this IEnumerable<T> collection, Func<T, ConsoleString> keySelector = null, MenuLabeling labeling = MenuLabeling.NumbersAndLetters, MenuCleanup cleanup = MenuCleanup.None, ConsoleString cancelKey = null, T cancelValue = default(T))
+        {
+            if (collection == null)
+                throw new ArgumentNullException(nameof(collection));
+            if (!collection.Any())
+                throw new ArgumentOutOfRangeException(nameof(collection), $"{nameof(MenuSelect)} can not on be executed on non-empty collections.");
+
+            if (keySelector == null)
+                keySelector = x => x.ToString();
+
+            MenuOption<T> result = null;
+
+            Console.CursorVisible = false;
+
+            using (var display = new MenuDisplay<MenuOption<T>>())
+            {
+                display.Cleanup = cleanup == MenuCleanup.None ? InputCleanup.None : InputCleanup.Clean;
+                display.PrefixesTop.SetKeys(labeling);
+
+                foreach (var item in collection)
+                    display.Options.Add(new MenuOption<T>(keySelector(item), item));
+
+                if (cancelKey != null || cancelValue != null)
+                {
+                    if (labeling != MenuLabeling.None)
+                        display.PrefixesBottom.SetKeys(new char[] { '0' });
+                    if (cancelKey == null) cancelKey = keySelector(cancelValue);
+                    display.Options.Add(new MenuOption<T>(cancelKey, cancelValue));
+                }
+
+                display.SelectedIndex = 0;
+
+                bool done = false;
+                ConsoleKeyInfo info;
+                do
+                {
+                    info = Console.ReadKey(true);
+                    switch (info.Key)
+                    {
+                        case ConsoleKey.DownArrow:
+                        case ConsoleKey.UpArrow:
+                            display.HandleKey(info);
+                            break;
+
+                        case ConsoleKey.Enter:
+                            result = display.Options[display.SelectedIndex];
+                            done = true;
+                            break;
+
+                        default:
+                            var index = display.IndexFromPrefix(info.KeyChar);
+                            if (index >= 0)
+                            {
+                                display.HandleKey(info);
+                                result = display.Options[index];
+                                done = true;
+                            }
+                            break;
+                    }
+                } while (!done);
+
+                if (cleanup == MenuCleanup.None)
+                    ColorConsole.CursorPosition = display.Origin + new ConsoleSize(0, display.Options.Count);
+            }
+
+            if (cleanup == MenuCleanup.RemoveMenuShowChoice)
+                ColorConsole.WriteLine(result.Text);
+
+            Console.CursorVisible = true;
+
+            return result.Value;
+        }
+        /// <summary>
+        /// Displays a menu where a single element can be selected from the collection.
+        /// Displays the key part of each element in the menu and returns the selected value part.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the Key part of elements in <paramref name="collection"/>.</typeparam>
+        /// <typeparam name="TValue">The type of the Value part of elements in <paramref name="collection"/>.</typeparam>
+        /// <param name="collection">The collection of element from which the menu should be created.</param>
+        /// <param name="labeling">The type of labeling (option prefix) that should be applied when displaying the menu.</param>
+        /// <param name="cleanup">The cleanup applied after displaying the menu.</param>
+        /// <param name="cancelKey">If not <c>null</c>, this string is displayed as a "cancel option" in the bottom of the menu.
+        /// <paramref name="cancelValue"/> will be returned if this option is selected.</param>
+        /// <param name="cancelValue">The value returned if <paramref name="cancelKey"/> is not <c>null</c>.</param>
+        /// <returns>The element that was selected using the displayed menu.</returns>
+        public static TValue MenuSelect<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> collection, MenuLabeling labeling = MenuLabeling.NumbersAndLetters, MenuCleanup cleanup = MenuCleanup.None, ConsoleString cancelKey = null, TValue cancelValue = default(TValue))
+        {
+            return MenuSelect(collection, x => x.Key.ToString(), labeling, cleanup, cancelKey, new KeyValuePair<TKey, TValue>(default(TKey), cancelValue)).Value;
         }
 
         /// <summary>
