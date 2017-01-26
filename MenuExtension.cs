b36241b3571fafbad8f6dec87c17db37655ef94a
deaddog <mikkelslsl@gmail.com>
2017-01-26 14:43:50 +0100

    Created new MenuSelectMultiple extension methods that unifies the previous extensions.

--- MenuExtension.cs
+++ MenuExtension.cs
@@ -78,9 +78,9 @@ namespace CommandLineParsing
         /// <summary>
         /// Displays a menu where a single element can be selected from the collection.
         /// </summary>
-        /// <typeparam name="T">The type of element in <paramref name="collection"/>.</typeparam>
+        /// <typeparam name="T">The type of the elements in <paramref name="collection"/>.</typeparam>
         /// <param name="collection">The collection of element from which the menu should be created.</param>
-        /// <param name="keySelector">A function that gets the <see cref="string"/> that should be displayed for an item in the collection</param>
+        /// <param name="keySelector">A function that gets the <see cref="ConsoleString"/> that should be displayed for an item in the collection.</param>
         /// <param name="labeling">The type of labeling (option prefix) that should be applied when displaying the menu.</param>
         /// <param name="cleanup">The cleanup applied after displaying the menu.</param>
         /// <param name="cancelKey">If not <c>null</c>, this string is displayed as a "cancel option" in the bottom of the menu.
@@ -165,7 +165,7 @@ namespace CommandLineParsing
         /// </summary>
         /// <typeparam name="TKey">The type of the Key part of elements in <paramref name="collection"/>.</typeparam>
         /// <typeparam name="TValue">The type of the Value part of elements in <paramref name="collection"/>.</typeparam>
-        /// <param name="collection">The collection of element from which the menu should be created.</param>
+        /// <param name="collection">The collection of elements from which the menu should be created.</param>
         /// <param name="labeling">The type of labeling (option prefix) that should be applied when displaying the menu.</param>
         /// <param name="cleanup">The cleanup applied after displaying the menu.</param>
         /// <param name="cancelKey">If not <c>null</c>, this string is displayed as a "cancel option" in the bottom of the menu.
@@ -268,6 +268,133 @@ namespace CommandLineParsing
             T[] arr = new T[res.Length];
             for (int i = 0; i < res.Length; i++) arr[i] = res[i].Value;
             return arr;
+        }
+
+        /// <summary>
+        /// Displays a menu where a set of elements can be selected from the collection.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements in <paramref name="collection"/>.</typeparam>
+        /// <param name="minimum">The minimum number of elements that must be selected for the function to return.</param>
+        /// <param name="maximum">The maximum number of elements that must be selected for the function to return.</param>
+        /// <param name="collection">The collection of element from which the menu should be created.</param>
+        /// <param name="onKeySelector">A function that gets the <see cref="ConsoleString"/> that should be displayed for an item in the collection, when the option is selected.</param>
+        /// <param name="offKeySelector">A function that gets the <see cref="ConsoleString"/> that should be displayed for an item in the collection, when the option is not selected.</param>
+        /// <param name="selected">A function that returns a boolean value indicating if an element should be pre-selected when the menu is displayed.</param>
+        /// <param name="labeling">The type of labeling (option prefix) that should be applied when displaying the menu.</param>
+        /// <param name="cleanup">The cleanup applied after displaying the menu.</param>
+        /// <param name="doneText">The <see cref="ConsoleString"/> that is displayed as the bottommost option in the menu. Selecting this option will cause the function to return.</param>
+        /// <returns>The elements that were selected using the displayed menu.</returns>
+        public static T[] MenuSelectMultiple<T>(this IEnumerable<T> collection, uint minimum = uint.MinValue, uint maximum = uint.MaxValue, Func<T, ConsoleString> onKeySelector = null, Func<T, ConsoleString> offKeySelector = null, Func<T, bool> selected = null, MenuLabeling labeling = MenuLabeling.NumbersAndLetters, MenuCleanup cleanup = MenuCleanup.None, ConsoleString doneText = null)
+        {
+            if (collection == null)
+                throw new ArgumentNullException(nameof(collection));
+            var items = collection.ToArray();
+            if (!items.Any())
+                throw new ArgumentOutOfRangeException(nameof(collection), $"{nameof(MenuSelect)} can not on be executed on non-empty collections.");
+
+            if (maximum < minimum)
+                throw new ArgumentOutOfRangeException(nameof(maximum), "The maximum selectable number of elements cannot be less then the minimum.");
+            if (minimum > items.Count())
+                throw new ArgumentOutOfRangeException(nameof(minimum), "The minimum selectable number of elements cannot be greater than the number of elements.");
+
+            if (onKeySelector == null)
+                onKeySelector = x => x.ToString();
+            if (offKeySelector == null)
+                offKeySelector = x =>
+                {
+                    var text = onKeySelector(x);
+                    if (text.HasColors)
+                        return text.ClearColors();
+                    else
+                        return new ConsoleString($"[DarkGray:{text.Content}]");
+                };
+            if (selected == null)
+                selected = x => false;
+
+            List<OnOffOption<T>> result = null;
+
+            Console.CursorVisible = false;
+
+            using (var display = new MenuDisplay<OnOffOption<T>>())
+            {
+                display.Cleanup = cleanup == MenuCleanup.None ? InputCleanup.None : InputCleanup.Clean;
+                display.PrefixesTop.SetKeys(labeling);
+                display.PrefixesBottom.SetKeys(new char[] { '0' });
+
+                foreach (var item in items)
+                    display.Options.Add(new OnOffOption<T>(onKeySelector(item), offKeySelector(item), selected(item), item));
+
+                if (doneText == null)
+                    doneText = "Done";
+                var doneOption = new OnOffOption<T>(doneText, $"[DarkGray:{doneText.Content}]", false, default(T));
+                display.Options.Add(doneOption);
+
+                display.SelectedIndex = 0;
+
+                bool done = false;
+                ConsoleKeyInfo info;
+                do
+                {
+                    info = Console.ReadKey(true);
+                    switch (info.Key)
+                    {
+                        case ConsoleKey.DownArrow:
+                        case ConsoleKey.UpArrow:
+                            display.HandleKey(info);
+                            break;
+
+                        case ConsoleKey.Enter:
+                        default:
+                            var index = info.Key == ConsoleKey.Enter ? display.SelectedIndex : display.IndexFromPrefix(info.KeyChar);
+
+                            if (index >= 0)
+                            {
+                                var option = display.Options[index];
+                                if (option == doneOption && doneOption.On)
+                                    done = true;
+                                else if (option != doneOption)
+                                {
+                                    option.On = !option.On;
+
+                                    var onCount = display.Options.Count(x => x != doneOption && x.On);
+                                    doneOption.On = onCount >= minimum && onCount <= maximum;
+                                }
+                            }
+                            break;
+                    }
+                } while (!done);
+
+                result = new List<OnOffOption<T>>(display.Options.Where(x => x != doneOption && x.On));
+
+                if (cleanup == MenuCleanup.None)
+                    ColorConsole.CursorPosition = display.Origin + new ConsoleSize(0, display.Options.Count);
+            }
+
+            if (cleanup == MenuCleanup.RemoveMenuShowChoice)
+                foreach (var r in result)
+                    ColorConsole.WriteLine(r.Text);
+
+            Console.CursorVisible = true;
+
+            return result.Select(x => x.Value).ToArray();
+        }
+        /// <summary>
+        /// Displays a menu where a set of elements can be selected from the collection.
+        /// Displays the key part of each element in the menu and returns the selected value part.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the Key part of elements in <paramref name="collection"/>.</typeparam>
+        /// <typeparam name="TValue">The type of the Value part of elements in <paramref name="collection"/>.</typeparam>
+        /// <param name="minimum">The minimum number of elements that must be selected for the function to return.</param>
+        /// <param name="maximum">The maximum number of elements that must be selected for the function to return.</param>
+        /// <param name="collection">The collection of elements from which the menu should be created.</param>
+        /// <param name="selected">A function that returns a boolean value indicating if an element should be pre-selected when the menu is displayed.</param>
+        /// <param name="labeling">The type of labeling (option prefix) that should be applied when displaying the menu.</param>
+        /// <param name="cleanup">The cleanup applied after displaying the menu.</param>
+        /// <param name="doneText">The <see cref="ConsoleString"/> that is displayed as the bottommost option in the menu. Selecting this option will cause the function to return.</param>
+        /// <returns>The elements that were selected using the displayed menu.</returns>
+        public static TValue[] MenuSelectMultiple<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> collection, uint minimum = uint.MinValue, uint maximum = uint.MaxValue, Func<KeyValuePair<TKey, TValue>, bool> selected = null, MenuLabeling labeling = MenuLabeling.NumbersAndLetters, MenuCleanup cleanup = MenuCleanup.None, ConsoleString doneText = null)
+        {
+            return MenuSelectMultiple(collection, minimum, maximum, null, null, selected, labeling, cleanup, doneText).Select(x => x.Value).ToArray();
         }
 
         /// <summary>
