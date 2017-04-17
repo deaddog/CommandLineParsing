ff7b59c3a7d1ed7af72199cb44583408367798be
deaddog <mikkelslsl@gmail.com>
2017-04-17 20:49:42 +0200

    Created gender+name dtos.

---
+++ CommandLineParsing.Tests/RandomNames/Genders.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommandLineParsing.Tests.RandomNames
+{
+    [Flags]
+    public enum Genders
+    {
+        Male = 1,
+        Female = 2,
+        Both = 3
+    }
+}
---
+++ CommandLineParsing.Tests/RandomNames/Name.cs
@@ -0,0 +1,21 @@
+﻿namespace CommandLineParsing.Tests.RandomNames
+{
+    public class Name
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public Genders Gender { get; }
+
+        public Name(string firstname, string lastname, Genders gender)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} ({Gender})";
+        }
+    }
+}
