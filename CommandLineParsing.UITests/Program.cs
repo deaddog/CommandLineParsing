8fc11fab7e4a4d293a5f507a91c6e1ba22d413a1
deaddog <mikkelslsl@gmail.com>
2017-04-23 20:01:38 +0200

    UI tests can be asserted using the NUnit assert methods.

--- CommandLineParsing.UITests/Program.cs
+++ CommandLineParsing.UITests/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -14,49 +16,38 @@ namespace CommandLineParsing.UITests
             var assembly = Assembly.GetExecutingAssembly();
             var tests = assembly.GetTypes().Where(IsTest).ToList();
 
+            var results = new Dictionary<Type, bool>();
+
             foreground = Console.ForegroundColor;
             background = Console.BackgroundColor;
 
             foreach (var t in tests)
             {
-                var name = t.GetCustomAttribute<TestNameAttribute>()?.Name ?? t.Name;
-
-                Console.ForegroundColor = foreground;
-                Console.BackgroundColor = background;
-                Console.CursorVisible = true;
-                Console.Clear();
-
-                Console.Write("Press any key to start executing test ");
-                Console.ForegroundColor = ConsoleColor.DarkCyan;
-                Console.Write(name);
-                Console.ReadKey(true);
-
-                Console.ForegroundColor = foreground;
-                Console.Clear();
-
-                UITest test = null;
-                try
-                {
-                    test = (UITest)Activator.CreateInstance(t);
-                }
-                catch (Exception e)
-                {
-                    ShowStateAndPressAnyKey("Test could not be loaded due to an exception: " + e.Message, ConsoleColor.Red);
-                    continue;
-                }
-
-                try
-                {
-                    test.Execute();
-                }
-                catch (Exception e)
-                {
-                    ShowStateAndPressAnyKey("Test execution failed due to an exception: " + e.Message, ConsoleColor.Red);
-                    continue;
-                }
-
-                ShowStateAndPressAnyKey("Test executed successfully!", ConsoleColor.Green);
+                results.Add(t, ExecuteTest(t));
             }
+
+            Console.ForegroundColor = foreground;
+            Console.BackgroundColor = background;
+            Console.CursorVisible = true;
+            Console.Clear();
+
+            var passedCount = results.Values.Count(x => x);
+            var failedCount = results.Values.Count(x => !x);
+            ColorConsole.WriteLine($"Tests results: [green:{passedCount} passed], [red:{failedCount} failed]");
+
+            if (failedCount > 0)
+            {
+                ColorConsole.WriteLine("Failed tests:");
+                foreach (var t in results.Where(x => !x.Value))
+                {
+                    var name = t.Key.GetCustomAttribute<TestNameAttribute>()?.Name ?? t.Key.Name;
+                    ColorConsole.WriteLine("- " + name);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
         }
 
         private static bool IsTest(Type type)
@@ -71,6 +62,53 @@ namespace CommandLineParsing.UITests
                 return true;
             else
                 return IsTestBase(type.BaseType);
+        }
+
+        private static bool ExecuteTest(Type t)
+        {
+            var name = t.GetCustomAttribute<TestNameAttribute>()?.Name ?? t.Name;
+
+            Console.ForegroundColor = foreground;
+            Console.BackgroundColor = background;
+            Console.CursorVisible = true;
+            Console.Clear();
+
+            Console.Write("Press any key to start executing test ");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write(name);
+            Console.ReadKey(true);
+
+            Console.ForegroundColor = foreground;
+            Console.Clear();
+
+            UITest test = null;
+            try
+            {
+                test = (UITest)Activator.CreateInstance(t);
+            }
+            catch (Exception e)
+            {
+                ShowStateAndPressAnyKey("Test could not be loaded due to an exception: " + e.Message, ConsoleColor.Red);
+                return false;
+            }
+
+            try
+            {
+                test.Execute();
+            }
+            catch (AssertionException e)
+            {
+                ShowStateAndPressAnyKey("=================\n   Test failed   \n=================\n" + e.Message, ConsoleColor.Red);
+                return false;
+            }
+            catch (Exception e)
+            {
+                ShowStateAndPressAnyKey("Test execution failed due to an exception: " + e.Message, ConsoleColor.Red);
+                return false;
+            }
+
+            ShowStateAndPressAnyKey("Test executed successfully!", ConsoleColor.Green);
+            return true;
         }
 
         private static void ShowStateAndPressAnyKey(string message, ConsoleColor? foregroundColor = null, ConsoleColor? backgroundColor = null)
