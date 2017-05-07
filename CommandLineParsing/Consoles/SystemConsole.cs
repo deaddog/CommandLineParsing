4c33886bfcd91e76935232d0bb20a85768d5dd04
deaddog <mikkelslsl@gmail.com>
2017-05-07 14:44:18 +0200

    SystemConsole implements the IConsole interface. This implements the functionality of #57.

--- CommandLineParsing/Consoles/SystemConsole.cs
+++ CommandLineParsing/Consoles/SystemConsole.cs
@@ -15,5 +15,75 @@ namespace CommandLineParsing.Consoles
         public static SystemConsole Instance => _singleton;
 
         private SystemConsole() { }
+
+        int IConsole.BufferWidth
+        {
+            get { return Console.BufferWidth; }
+            set { Console.BufferWidth = value; }
+        }
+        int IConsole.BufferHeight
+        {
+            get { return Console.BufferHeight; }
+            set { Console.BufferHeight = value; }
+        }
+        void IConsole.SetBufferSize(int width, int height) => Console.SetBufferSize(width, height);
+
+        int IConsole.CursorLeft
+        {
+            get { return Console.CursorLeft; }
+            set { Console.CursorLeft = value; }
+        }
+        int IConsole.CursorTop
+        {
+            get { return Console.CursorTop; }
+            set { Console.CursorTop = value; }
+        }
+        void IConsole.SetCursorPosition(int left, int top) => Console.SetCursorPosition(left, top);
+
+        int IConsole.WindowWidth
+        {
+            get { return Console.WindowWidth; }
+            set { Console.WindowWidth = value; }
+        }
+        int IConsole.WindowHeight
+        {
+            get { return Console.WindowHeight; }
+            set { Console.WindowHeight = value; }
+        }
+        void IConsole.SetWindowSize(int width, int height) => Console.SetWindowSize(width, height);
+
+        int IConsole.WindowLeft
+        {
+            get { return Console.WindowLeft; }
+            set { Console.WindowLeft = value; }
+        }
+        int IConsole.WindowTop
+        {
+            get { return Console.WindowTop; }
+            set { Console.WindowTop = value; }
+        }
+        void IConsole.SetWindowPosition(int left, int top) => Console.SetWindowPosition(left, top);
+
+        bool IConsole.CursorVisible
+        {
+            get { return Console.CursorVisible; }
+            set { Console.CursorVisible = value; }
+        }
+        ConsoleColor IConsole.ForegroundColor
+        {
+            get { return Console.ForegroundColor; }
+            set { Console.ForegroundColor = value; }
+        }
+        ConsoleColor IConsole.BackgroundColor
+        {
+            get { return Console.BackgroundColor; }
+            set { Console.BackgroundColor = value; }
+        }
+        void IConsole.ResetColor() => Console.ResetColor();
+
+        void IConsole.Write(string value) => Console.Write(value);
+        void IConsole.WriteLine(string value) => Console.WriteLine(value);
+        ConsoleKeyInfo IConsole.ReadKey(bool intercept) => Console.ReadKey(intercept);
+        string IConsole.ReadLine() => Console.ReadLine();
     }
 }
