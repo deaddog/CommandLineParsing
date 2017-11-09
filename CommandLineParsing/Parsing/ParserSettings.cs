23d2eb05fa2154aac23e6192df8bc51a3ab58dbc
deaddog <mikkelslsl@gmail.com>
2017-11-09 22:49:15 +0100

    Parser settings were moved to new ParserSettings class. SmartParser i functionality only.

---
+++ CommandLineParsing/Parsing/ParserSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CommandLineParsing.Parsing
+{
+    internal class ParserSettings
+    {
+        public bool EnumIgnoreCase { get; set; }
+        public string NoParserExceptionMessage { get; set; }
+        public Message NoValueMessage { get; set; }
+        public Message MultipleValuesMessage { get; set; }
+        public Func<string, Message> TypeErrorMessage { get; set; }
+        public bool UseParserMessage { get; set; }
+    }
+}
--- CommandLineParsing/Parsing/SmartParser.cs
+++ CommandLineParsing/Parsing/SmartParser.cs
@@ -2,131 +2,80 @@
 
 namespace CommandLineParsing.Parsing
 {
-    internal class SmartParser<T>
+    internal static class SmartParser
     {
-        private ParameterTryParse<T> parser;
-        private bool enumIgnore;
-        private string noParserExceptionMessage;
-
-        private Message noValueMessage;
-        private Message multipleValuesMessage;
-        private Func<string, Message> typeErrorMessage;
-        private bool useParserMessage;
-
-        public SmartParser()
+        public static Message Parse<T>(ParserSettings settings, string[] args, out T result)
         {
-        }
-
-        public ParameterTryParse<T> Parser
-        {
-            get { return parser; }
-            set { parser = value; }
-        }
-
-        public bool EnumIgnoreCase
-        {
-            get { return enumIgnore; }
-            set { enumIgnore = value; }
-        }
-        public string NoParserExceptionMessage
-        {
-            get { return noParserExceptionMessage; }
-            set { noParserExceptionMessage = value; }
-        }
-        public Message NoValueMessage
-        {
-            get { return noValueMessage; }
-            set { noValueMessage = value; }
-        }
-        public Message MultipleValuesMessage
-        {
-            get { return multipleValuesMessage; }
-            set { multipleValuesMessage = value; }
-        }
-        public Func<string, Message> TypeErrorMessage
-        {
-            get { return typeErrorMessage; }
-            set { typeErrorMessage = value; }
-        }
-        public bool UseParserMessage
-        {
-            get { return useParserMessage; }
-            set { useParserMessage = value; }
-        }
-
-        public Message Parse(string[] args, out T result)
-        {
-            if (parser != null)
-                return parser(args, out result);
-            else if (typeof(T).IsArray)
+            if (typeof(T).IsArray)
             {
                 var arrayType = typeof(T).GetElementType();
 
-                var arrMethod = GetType()
+                var arrMethod = typeof(SmartParser)
                     .GetMethod(nameof(ParseArray), System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
                     .MakeGenericMethod(arrayType);
 
-                var arrMethodArgs = new object[2];
-                arrMethodArgs[0] = args;
+                var arrMethodArgs = new object[3];
+                arrMethodArgs[0] = settings;
+                arrMethodArgs[1] = args;
 
-                var msg = arrMethod.Invoke(this, arrMethodArgs);
-                result = (T)arrMethodArgs[1];
+                var msg = arrMethod.Invoke(null, arrMethodArgs);
+                result = (T)arrMethodArgs[2];
                 return (Message)msg;
             }
             else
-                return ParseSingle(args, out result);
+                return ParseSingle(settings, args, out result);
         }
 
-        private Message ParseSingle(string[] args, out T result)
+        private static Message ParseSingle<T>(ParserSettings settings, string[] args, out T result)
         {
             result = default(T);
 
             if (args.Length == 0)
             {
-                if (noValueMessage.IsError)
-                    return noValueMessage;
+                if (settings.NoValueMessage.IsError)
+                    return settings.NoValueMessage;
                 else
                     args = new string[] { string.Empty };
             }
             else if (args.Length > 1)
-                return multipleValuesMessage;
+                return settings.MultipleValuesMessage;
 
             if (ParserLookup.Table.HasMessageTryParse<T>())
             {
                 var msg = ParserLookup.Table.MessageTryParse(args[0], out result);
                 if (msg.IsError)
-                    return useParserMessage ? msg : typeErrorMessage(args[0]);
+                    return settings.UseParserMessage ? msg : settings.TypeErrorMessage(args[0]);
                 else
                     return Message.NoError;
             }
-            else if (ParserLookup.Table.HasTryParse<T>(enumIgnore))
-                if (!ParserLookup.Table.TryParse(enumIgnore, args[0], out result))
-                    return typeErrorMessage(args[0]);
+            else if (ParserLookup.Table.HasTryParse<T>(settings.EnumIgnoreCase))
+                if (!ParserLookup.Table.TryParse(settings.EnumIgnoreCase, args[0], out result))
+                    return settings.TypeErrorMessage(args[0]);
                 else
                     return Message.NoError;
             else
-                throw new InvalidOperationException(noParserExceptionMessage);
+                throw new InvalidOperationException(settings.NoParserExceptionMessage);
         }
-        private Message ParseArray<TParse>(string[] args, out TParse[] result)
+        private static Message ParseArray<T>(ParserSettings settings, string[] args, out T[] result)
         {
-            var arr = new TParse[args.Length];
+            var arr = new T[args.Length];
             result = null;
 
-            if (ParserLookup.Table.HasMessageTryParse<TParse>())
+            if (ParserLookup.Table.HasMessageTryParse<T>())
                 for (int i = 0; i < args.Length; i++)
                 {
                     Message msg = ParserLookup.Table.MessageTryParse(args[i], out arr[i]);
                     if (msg.IsError)
-                        return useParserMessage ? msg : typeErrorMessage(args[i]);
+                        return settings.UseParserMessage ? msg : settings.TypeErrorMessage(args[i]);
                 }
-            else if (ParserLookup.Table.HasTryParse<TParse>(enumIgnore))
+            else if (ParserLookup.Table.HasTryParse<T>(settings.EnumIgnoreCase))
                 for (int i = 0; i < args.Length; i++)
                 {
-                    if (!ParserLookup.Table.TryParse(enumIgnore, args[i], out arr[i]))
-                        return typeErrorMessage(args[i]);
+                    if (!ParserLookup.Table.TryParse(settings.EnumIgnoreCase, args[i], out arr[i]))
+                        return settings.TypeErrorMessage(args[i]);
                 }
             else
-                throw new InvalidOperationException(noParserExceptionMessage);
+                throw new InvalidOperationException(settings.NoParserExceptionMessage);
 
             result = arr;
             return Message.NoError;
