9c0ce3dfa73004796f45b3508adda9cdd8af4e2b
deaddog <mikkelslsl@gmail.com>
2017-11-17 20:36:43 +0100

    Added additional tests for basic string parsing.

--- CommandLineParsing.Tests/Output/ConsoleStringTests.cs
+++ CommandLineParsing.Tests/Output/ConsoleStringTests.cs
@@ -14,5 +14,22 @@ namespace CommandLineParsing.Tests.Output
             Assert.AreEqual(ConsoleString.Empty, ConsoleString.Parse(string.Empty, false));
             Assert.AreEqual(ConsoleString.Empty, new ConsoleString(new[] { new ConsoleStringSegment(string.Empty) }));
         }
+
+        [Test]
+        public void ParseStringFlatten()
+        {
+            Assert.AreEqual(ConsoleString.Parse("test1test2"), new ConsoleString(new[] { new ConsoleStringSegment("test1"), new ConsoleStringSegment("test2") }));
+            Assert.AreEqual(ConsoleString.Parse("[red:test1test2]"), ConsoleString.Parse("[red:test1][red:test2]"));
+            Assert.AreEqual(ConsoleString.Parse("[blue:test1][red:test2]test3"), ConsoleString.Parse("[blue:test1][red:test2]test3"));
+            Assert.AreEqual(ConsoleString.Parse("[blue:test1][red:test2test3]test4"), ConsoleString.Parse("[blue:test1][red:test2][red:test3]test4"));
+        }
+
+        [Test]
+        public void ParseNoColor()
+        {
+            Assert.AreEqual(ConsoleString.Empty, ConsoleString.Parse("[:]"));
+            Assert.AreEqual(ConsoleString.Parse("test1"), ConsoleString.Parse("[:test1]"));
+            Assert.AreEqual(ConsoleString.Parse("test1"), ConsoleString.Parse("[ :test1]"));
+        }
     }
 }
