2a77a8cd5150bcbf66df6e7e8bc5afe524eb7188
deaddog <mikkelslsl@gmail.com>
2017-11-17 18:51:30 +0100

    Added a very simple test for ConsoleString parsing.

---
+++ CommandLineParsing.Tests/Output/ConsoleStringTests.cs
@@ -0,0 +1,16 @@
+﻿using CommandLineParsing.Output;
+using NUnit.Framework;
+
+namespace CommandLineParsing.Tests.Output
+{
+    [TestFixture]
+    public class ConsoleStringTests
+    {
+        [Test]
+        public void ParseEmptyString()
+        {
+            Assert.AreEqual(new ConsoleString(), new ConsoleString(string.Empty, true));
+            Assert.AreEqual(new ConsoleString(), new ConsoleString(string.Empty, false));
+        }
+    }
+}
