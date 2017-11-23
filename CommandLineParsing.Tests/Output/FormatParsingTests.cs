2433951c59c3fb9c1ac77f1409ac6aa9ee37378f
deaddog <mikkelslsl@gmail.com>
2017-11-23 20:35:27 +0100

    Created a simple format parsing unit test.

---
+++ CommandLineParsing.Tests/Output/FormatParsingTests.cs
@@ -0,0 +1,15 @@
+﻿using CommandLineParsing.Output.Formatting.Structure;
+using NUnit.Framework;
+
+namespace CommandLineParsing.Tests.Output
+{
+    [TestFixture]
+    public class FormatParsingTests
+    {
+        [Test]
+        public void ParseEmptyString()
+        {
+            Assert.AreEqual(new FormatText(string.Empty), FormatElement.Parse(string.Empty));
+        }
+    }
+}
