4f809a2b9d285cc45e1e90e34abcd74586cda6c3
deaddog <mikkelslsl@gmail.com>
2017-11-27 23:41:09 +0100

    Added format color-syntax parsing tests.

--- CommandLineParsing.Tests/Output/FormatParsingTests.cs
+++ CommandLineParsing.Tests/Output/FormatParsingTests.cs
@@ -11,5 +11,40 @@ namespace CommandLineParsing.Tests.Output
         {
             Assert.AreEqual(FormatNoContent.Element, FormatElement.Parse(string.Empty));
         }
+
+        [Test]
+        public void ParseBasicString()
+        {
+            Assert.AreEqual(new FormatText("hello world"), FormatElement.Parse("hello world"));
+        }
+
+        [Test]
+        public void ParseColorString()
+        {
+            Assert.AreEqual(FormatNoContent.Element, FormatElement.Parse("[green:]"));
+            Assert.AreEqual(new FormatText("hello world"), FormatElement.Parse("[:hello world]"));
+            Assert.AreEqual(new FormatText("hello world"), FormatElement.Parse("[hello world]"));
+            Assert.AreEqual(new FormatColor("green", new FormatText("hello world")), FormatElement.Parse("[green:hello world]"));
+            Assert.AreEqual(new FormatColor("green", new FormatText("hello world")), FormatElement.Parse("[green:hello ][green:world]"));
+
+            Assert.AreEqual(new FormatConcatenation(new FormatElement[] {
+                    new FormatText("text1"),
+                    new FormatColor("green", new FormatText("hello world"))
+                }),
+                FormatElement.Parse("text1[green:hello world]"));
+
+            Assert.AreEqual(new FormatConcatenation(new FormatElement[] {
+                    new FormatColor("green", new FormatText("hello world")),
+                    new FormatText("text2")
+                }),
+                FormatElement.Parse("[green:hello world]text2"));
+
+            Assert.AreEqual(new FormatConcatenation(new FormatElement[] {
+                    new FormatText("text1"),
+                    new FormatColor("green", new FormatText("hello world")),
+                    new FormatText("text2")
+                }),
+                FormatElement.Parse("text1[green:hello world]text2"));
+        }
     }
 }
