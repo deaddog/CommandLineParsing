a33977a7f28d5035c28093ee7a805a6e0731399a
deaddog <mikkelslsl@gmail.com>
2019-08-03 22:46:05 +0200

    NameService is now based on static list.

--- CommandLineParsing.Tests/RandomNames/Genders.cs
+++ CommandLineParsing.Tests/RandomNames/Genders.cs
@@ -2,11 +2,9 @@
 
 namespace CommandLineParsing.Tests.RandomNames
 {
-    [Flags]
     public enum Genders
     {
-        Male = 1,
-        Female = 2,
-        Both = 3
+        Male,
+        Female
     }
 }
