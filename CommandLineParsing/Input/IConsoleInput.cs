d6b17b840a66eddeda4dfa8c449aa62325c900e2
deaddog <mikkelslsl@gmail.com>
2019-08-12 20:02:45 +0200

    Removed IDisposable and Cleanup from IConsoleInput.

--- CommandLineParsing/Input/IConsoleInput.cs
+++ CommandLineParsing/Input/IConsoleInput.cs
@@ -5,18 +5,13 @@ namespace CommandLineParsing.Input
     /// <summary>
     /// Provides methods for handling input in console applications.
     /// </summary>
-    public interface IConsoleInput : IDisposable
+    public interface IConsoleInput
     {
         /// <summary>
         /// Handles the specified key by updating internal/visual state.
         /// </summary>
         /// <param name="key">The key to process.</param>
         void HandleKey(ConsoleKeyInfo key);
-
-        /// <summary>
-        /// Gets the type of cleanup that should be applied when disposing the <see cref="IConsoleInput"/>.
-        /// </summary>
-        InputCleanup Cleanup { get; }
 
         /// <summary>
         /// Gets the location where the input is displayed (upper-left corner).
