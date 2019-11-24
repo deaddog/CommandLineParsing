3042698d4c90d3309ebc77937fb9bf74167d087a
deaddog <mikkelslsl@gmail.com>
2019-11-24 20:23:52 +0100

    Defined basic color model matching current needs.

---
+++ CommandLineParsing/Output/Color.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CommandLineParsing.Output
+{
+    /// <summary>
+    /// Represents the usage of a specific color in a string format (see <see cref="ConsoleString"/>.
+    /// </summary>
+    public class Color
+    {
+        /// <summary>
+        /// Initializes a new <see cref="Color"/> instance.
+        /// </summary>
+        /// <param name="name">The name used to describe the desired color.</param>
+        public Color(string name)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+        }
+
+        /// <summary>
+        /// Gets the name used for the color.
+        /// </summary>
+        public string Name { get; }
+    }
+}
