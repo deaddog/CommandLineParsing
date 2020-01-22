e5f07e900451c3972f8ed703c87fd730cfca874d
deaddog <mikkelslsl@gmail.com>
2020-01-22 17:38:14 +0100

    Defined tests for the color parse method.

---
+++ CommandLineParsing.Tests/Output/ColorTests.cs
@@ -0,0 +1,40 @@
+﻿using CommandLineParsing.Output;
+using NUnit.Framework;
+
+namespace CommandLineParsing.Tests.Output
+{
+    [TestFixture]
+    public class ColorTests
+    {
+        [Test]
+        public void ParseEmptyString()
+        {
+            Assert.AreEqual(Color.NoColor, Color.Parse(""));
+            Assert.AreEqual(Color.NoColor, Color.Parse("|"));
+            Assert.AreEqual(Color.NoColor, Color.Parse("  |"));
+            Assert.AreEqual(Color.NoColor, Color.Parse("|  "));
+            Assert.AreEqual(Color.NoColor, Color.Parse("  |  "));
+        }
+
+        [Test]
+        public void ParseForegroundOnlyString()
+        {
+            var red = Color.NoColor.WithForeground("red");
+
+            Assert.AreEqual(red, Color.Parse("red"));
+            Assert.AreEqual(red, Color.Parse("red|"));
+            Assert.AreEqual(red, Color.Parse("  red"));
+            Assert.AreEqual(red, Color.Parse("  red| "));
+            Assert.AreEqual(red, Color.Parse("  red  "));
+        }
+
+        [Test]
+        public void ParseBackgroundOnlyString()
+        {
+            var red = Color.NoColor.WithBackground("red");
+
+            Assert.AreEqual(red, Color.Parse("|red"));
+            Assert.AreEqual(red, Color.Parse(" |  red"));
+        }
+    }
+}
