50cdd3f583199ce1dc64b8879afb3619b9162ccc
deaddog <mikkelslsl@gmail.com>
2020-01-25 22:33:22 +0100

    Added assertion methods for the full buffer/window state.

--- CommandLineParsing.Tests/ConsoleAssertions.cs
+++ CommandLineParsing.Tests/ConsoleAssertions.cs
@@ -1,6 +1,7 @@
 ﻿using CommandLineParsing.Output;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CommandLineParsing.Tests
@@ -28,7 +29,7 @@ namespace CommandLineParsing.Tests
         {
             var state = GetState(strings);
 
-            Assert.AreEqual(expectedCount, state.Length, "Number of lines in console ({0}) did not match expected ({1}).", state.Length, expectedCount);
+            Assert.AreEqual(expectedCount, state.Length, "Number of lines in console {2} ({0}) did not match expected ({1}).", state.Length, expectedCount, strings.ToString().ToLower());
         }
 
         public void Line(int lineIndex, ConsoleString expected, AssertStrings strings = AssertStrings.Buffer)
@@ -37,12 +38,34 @@ namespace CommandLineParsing.Tests
             var str = state.FirstOrDefault(x => x.Position.Top == lineIndex);
 
             if (str == null)
-                Assert.Fail("The console did not contain line index {0}.", lineIndex);
+                Assert.Fail("The console {1} did not contain line index {0}.", lineIndex, strings.ToString().ToLower());
             else
             {
                 Assert.AreEqual(0, str.Position.Left);
                 Assert.AreEqual(expected, str);
             }
+        }
+        public void Lines(IEnumerable<ConsoleString> expected, AssertStrings strings = AssertStrings.Buffer)
+        {
+            var lines = expected.ToArray();
+            LineCount(lines.Length, strings);
+
+            for (int l = 0; l < lines.Length; l++)
+                Line(l, lines[l], strings);
+        }
+
+        public void BufferAndWindowLines(params ConsoleString[] expected)
+        {
+            BufferLines(expected);
+            WindowLines(expected);
+        }
+        public void BufferLines(params ConsoleString[] expected)
+        {
+            Lines(expected, AssertStrings.Buffer);
+        }
+        public void WindowLines(params ConsoleString[] expected)
+        {
+            Lines(expected, AssertStrings.Window);
         }
     }
 }
