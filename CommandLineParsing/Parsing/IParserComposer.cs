8a3e19864857eece277ba7e3fde49add2fa78c8d
deaddog <mikkelslsl@gmail.com>
2020-09-13 15:37:19 +0200

    Defined parser and composer interfaces.

---
+++ CommandLineParsing/Parsing/IParserComposer.cs
@@ -0,0 +1,8 @@
+﻿namespace CommandLineParsing.Parsing
+{
+    public interface IParserComposer<T, out TReturn>
+    {
+        IParser<T> Parser { get; }
+        TReturn WithParser(IParser<T> parser);
+    }
+}
