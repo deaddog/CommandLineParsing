a9ec063ecfe28d560d6a789549c95692ee78d67d
deaddog <mikkelslsl@gmail.com>
2020-09-13 17:52:55 +0200

    Defined configuration objects for console readline.

---
+++ CommandLineParsing/Input/Reading/ReadlineCleanupConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace CommandLineParsing.Input.Reading
+{
+    public class ReadlineCleanupConfiguration
+    {
+        public ReadlineCleanupConfiguration(ReadLineCleanup success, ReadLineCleanup cancel)
+        {
+            Success = success;
+            Cancel = cancel;
+        }
+
+        public ReadLineCleanup Success { get; }
+        public ReadLineCleanup Cancel { get; }
+    }
+}
---
+++ CommandLineParsing/Input/Reading/ReadlineConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using CommandLineParsing.Output;
+using CommandLineParsing.Parsing;
+using CommandLineParsing.Validation;
+using System;
+
+namespace CommandLineParsing.Input.Reading
+{
+    public class ReadlineConfiguration<T> : IParserComposer<T, ReadlineConfiguration<T>>, IValidatorComposer<T, ReadlineConfiguration<T>>
+    {
+        public ReadlineConfiguration(ConsoleString prompt, string @default, ReadlineCleanupConfiguration cleanup, IParser<T> parser, IValidator<T> validator)
+        {
+            Prompt = prompt ?? throw new ArgumentNullException(nameof(prompt));
+            Default = @default ?? throw new ArgumentNullException(nameof(@default));
+            Cleanup = cleanup ?? throw new ArgumentNullException(nameof(cleanup));
+
+            Parser = parser ?? throw new ArgumentNullException(nameof(parser));
+            Validator = validator ?? throw new ArgumentNullException(nameof(validator));
+        }
+
+        public ConsoleString Prompt { get; }
+        public string Default { get; }
+        public ReadlineCleanupConfiguration Cleanup { get; }
+
+        public IParser<T> Parser { get; }
+        public ReadlineConfiguration<T> WithParser(IParser<T> parser)
+        {
+            return new ReadlineConfiguration<T>
+            (
+                prompt: Prompt,
+                @default: Default,
+                cleanup: Cleanup,
+                parser: parser,
+                validator: Validator
+            );
+        }
+
+        public IValidator<T> Validator { get; }
+        public ReadlineConfiguration<T> WithValidator(IValidator<T> validator)
+        {
+            return new ReadlineConfiguration<T>
+            (
+                prompt: Prompt,
+                @default: Default,
+                cleanup: Cleanup,
+                parser: Parser,
+                validator: validator
+            );
+        }
+    }
+}
