4cd468112db5bbce913206b690610cd9cc6b7812
deaddog <mikkelslsl@gmail.com>
2020-09-19 21:49:50 +0200

    Configuration type for reading passwords.

---
+++ CommandLineParsing/Input/Reading/ReadPasswordConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using CommandLineParsing.Output;
+using System;
+
+namespace CommandLineParsing.Input.Reading
+{
+    public class ReadPasswordConfiguration
+    {
+        public ReadPasswordConfiguration(ConsoleString prompt, ConsoleString renderAs, bool repeatRender)
+        {
+            Prompt = prompt ?? throw new ArgumentNullException(nameof(prompt));
+
+            RenderAs = renderAs ?? throw new ArgumentNullException(nameof(renderAs));
+            RepeatRender = repeatRender;
+        }
+
+        public ConsoleString Prompt { get; }
+
+        public ConsoleString RenderAs { get; }
+        public bool RepeatRender { get; }
+    }
+}
---
+++ CommandLineParsing/Input/Reading/ReadPasswordConfigurationExtensions.cs
@@ -0,0 +1,38 @@
+﻿using CommandLineParsing.Output;
+
+namespace CommandLineParsing.Input.Reading
+{
+    public static class ReadPasswordConfigurationExtensions
+    {
+        public static ReadPasswordConfiguration WithPrompt(this ReadPasswordConfiguration composer, ConsoleString prompt)
+        {
+            return new ReadPasswordConfiguration
+            (
+                prompt: prompt,
+                renderAs: composer.RenderAs,
+                repeatRender: composer.RepeatRender
+            );
+        }
+        public static ReadPasswordConfiguration WithoutPrompt(this ReadPasswordConfiguration composer) => composer.WithPrompt(ConsoleString.Empty);
+
+        public static ReadPasswordConfiguration WithRender(this ReadPasswordConfiguration composer, char render, bool repeat)
+        {
+            return new ReadPasswordConfiguration
+            (
+                prompt: composer.Prompt,
+                renderAs: ConsoleString.FromContent(render.ToString()),
+                repeatRender: repeat
+            );
+        }
+        public static ReadPasswordConfiguration WithRender(this ReadPasswordConfiguration composer, ConsoleString render, bool repeat)
+        {
+            return new ReadPasswordConfiguration
+            (
+                prompt: composer.Prompt,
+                renderAs: render,
+                repeatRender: repeat
+            );
+        }
+        public static ReadPasswordConfiguration WithoutRender(this ReadPasswordConfiguration composer) => composer.WithRender(ConsoleString.Empty, false);
+    }
+}
