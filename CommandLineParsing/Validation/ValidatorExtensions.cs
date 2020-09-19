0677c6e57e95c9bab7992202965b2fa1023535c2
deaddog <mikkelslsl@gmail.com>
2020-09-19 22:06:47 +0200

    Validator extension method for passing through parser output.

---
+++ CommandLineParsing/Validation/ValidatorExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace CommandLineParsing.Validation
+{
+    public static class ValidatorExtensions
+    {
+        public static Message<T> Validate<T>(this IValidator<T> validator, Message<T> message)
+        {
+            if (message.IsError)
+                return message;
+            else
+            {
+                var msg = validator.Validate(message.Value);
+
+                if (msg.IsError)
+                    return new Message<T>(msg.Content);
+                else
+                    return message;
+            }
+        }
+    }
+}
