771802579a117fa5bd87f6aeaa750c16768c617b
deaddog <mikkelslsl@gmail.com>
2020-09-19 22:42:19 +0200

    Extension for parsing single-string values.

---
+++ CommandLineParsing/Parsing/ParserExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace CommandLineParsing.Parsing
+{
+    public static class ParserExtensions
+    {
+        public static Message<T> Parse<T>(this IParser<T> parser, string input)
+        {
+            return parser.Parse(new string[] { input });
+        }
+    }
+}
