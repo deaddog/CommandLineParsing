5ac8c650863e4bc9c9d2ef7b5d8ee87782f1582b
deaddog <mikkelslsl@gmail.com>
2020-09-30 23:34:10 +0200

    A few extensions for the new configuration.

---
+++ CommandLineParsing/Input/Reading/ReadcharConfigurationExtensions.cs
@@ -0,0 +1,26 @@
+﻿﻿using CommandLineParsing.Output;
+
+namespace CommandLineParsing.Input.Reading
+{
+    public static class ReadcharConfigurationExtensions
+    {
+        public static ReadcharConfiguration<T> WithPrompt<T>(this ReadcharConfiguration<T> composer, ConsoleString prompt)
+        {
+            return new ReadcharConfiguration<T>
+            (
+                prompt: prompt,
+                options: composer.Options
+            );
+        }
+        public static ReadcharConfiguration<T> WithoutPrompt<T>(this ReadcharConfiguration<T> composer) => composer.WithPrompt(ConsoleString.Empty);
+
+        public static ReadcharConfiguration<T> WithOption<T>(this ReadcharConfiguration<T> composer, char character, T value)
+        {
+            return new ReadcharConfiguration<T>
+            (
+                prompt: composer.Prompt,
+                options: composer.Options.SetItem(character, value)
+            );
+        }
+    }
+}
