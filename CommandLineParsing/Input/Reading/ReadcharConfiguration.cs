36af561f574acda3a76b82d670f9a06a7fc89e59
deaddog <mikkelslsl@gmail.com>
2020-09-30 23:33:11 +0200

    Defined configuration for reading a single char.

---
+++ CommandLineParsing/Input/Reading/ReadcharConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using CommandLineParsing.Output;
+using System;
+using System.Collections.Immutable;
+
+namespace CommandLineParsing.Input.Reading
+{
+    public class ReadcharConfiguration<T>
+    {
+        public ReadcharConfiguration(ConsoleString prompt, IImmutableDictionary<char, T> options)
+        {
+            Prompt = prompt ?? throw new ArgumentNullException(nameof(prompt));
+            Options = options ?? throw new ArgumentNullException(nameof(options));
+        }
+
+        public ConsoleString Prompt { get; }
+        public IImmutableDictionary<char, T> Options { get; }
+    }
+}
