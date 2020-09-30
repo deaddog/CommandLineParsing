b9f7d6d66075576af9416ded4c5a71b7810699c8
deaddog <mikkelslsl@gmail.com>
2020-09-30 23:35:20 +0200

    Implemented Read and ReadOrCancel methods for char.

---
+++ CommandLineParsing/Input/Reading/ReadcharExtensions.cs
@@ -0,0 +1,55 @@
+﻿using CommandLineParsing.Output;
+using CommandLineParsing.Validation;
+using System.Linq;
+
+namespace CommandLineParsing.Input.Reading
+{
+    public static class ReadcharExtensions
+    {
+        public static T Read<T>(this ReadcharConfiguration<T> configuration, IConsole console)
+        {
+            if (configuration.Options.Count == 0)
+                throw new System.ArgumentException("Configuration must support at least one option.");
+
+            var start = console.GetCursorPosition();
+            console.Write(configuration.Prompt);
+
+            System.ConsoleKeyInfo input;
+            T value;
+            do
+            {
+                input = console.ReadKey(true);
+            } while (!configuration.Options.TryGetValue(input.KeyChar, out value));
+
+            console.WriteLine();
+
+            return value;
+        }
+        public static bool ReadOrCancel<T>(this ReadcharConfiguration<T> configuration, IConsole console, out T result)
+        {
+            if (configuration.Options.Count == 0)
+                throw new System.ArgumentException("Configuration must support at least one option.");
+
+            var start = console.GetCursorPosition();
+            console.Write(configuration.Prompt);
+
+            System.ConsoleKeyInfo input;
+            do
+            {
+                input = console.ReadKey(true);
+
+                if (input.Key == System.ConsoleKey.Escape)
+                {
+                    console.WriteLine();
+
+                    result = default;
+                    return false;
+                }
+            } while (!configuration.Options.TryGetValue(input.KeyChar, out result));
+
+            console.WriteLine();
+
+            return true;
+        }
+    }
+}
