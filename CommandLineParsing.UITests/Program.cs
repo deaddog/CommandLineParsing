629a1716e38c2ac7eac66f50c11718a9afaf26a5
deaddog <mikkelslsl@gmail.com>
2020-12-31 16:08:36 +0100

    TEMP

--- CommandLineParsing.UITests/Program.cs
+++ CommandLineParsing.UITests/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace CommandLineParsing.UITests
+﻿using System;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+
+namespace CommandLineParsing.UITests
 {
     class Program
     {
@@ -10,6 +14,56 @@
             IConsole c = Consoles.System;
 
             // Do stuff using the console, by interacting with ColorConsole.
+
+            var a1 = 
         }
+    }
+
+    public interface ICommand
+    {
+        Task ExecuteAsync(string[] args);
+    }
+
+    public class COMMAND : ICommand
+    {
+    }
+
+    public class CMD<TParams>
+    {
+        public TParams Parameters { get; }
+    }
+
+
+    public class C<TArgs>
+    {
+        public Task<Message> ExecuteAsync(TArgs arguments)
+        {
+
+        }
+    }
+
+
+
+
+    public class Parameters
+    {
+        public IParameter<int> Number { get; }
+        public IParameter<string> Name { get; }
+    }
+
+    public class Arguments
+    {
+        public IArgument<int> Number { get; }
+        public IArgument<string> Name { get; }
+    }
+
+    public interface IParameter<T>
+    {
+        IArgument<T> Resolve(string[] args);
+    }
+
+
+    public interface IArgument<T>
+    {
     }
 }
