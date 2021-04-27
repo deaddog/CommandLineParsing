4c70a4bc9215dd19893bdfe54221547902f9680b
deaddog <mikkelslsl@gmail.com>
2021-04-27 22:15:38 +0200

    Implementation of IConsole for the System.Console.

---
+++ ConsoleTools/Consoles.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleTools
+{
+    public static class Consoles
+    {
+        public static IConsole System { get; } = new SystemConsole();
+
+        private class SystemConsole : IConsole
+        {
+            public Vector BufferSize => (Console.BufferWidth, Console.BufferHeight);
+            public Vector CursorPosition
+            {
+                get => (Console.CursorLeft, Console.CursorTop);
+                set => Console.SetCursorPosition(value.Horizontal, value.Vertical);
+            }
+            public Vector WindowSize => (Console.WindowWidth, Console.WindowHeight);
+            public Vector WindowPosition => (Console.WindowLeft, Console.WindowHeight);
+
+            public ConsoleColor ForegroundColor
+            {
+                get => Console.ForegroundColor;
+                set => Console.ForegroundColor = value;
+            }
+            public ConsoleColor BackgroundColor
+            {
+                get => Console.BackgroundColor;
+                set => Console.BackgroundColor = value;
+            }
+            public void ResetColor() => Console.ResetColor();
+
+            public void Render(string value) => Console.Write(value);
+            public ConsoleKeyInfo ReadKey(bool intercept) => Console.ReadKey(intercept);
+        }
+    }
+}
