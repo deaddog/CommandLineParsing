0b75405912cdca94d018ecdedf26374c8056afa4
deaddog <mikkelslsl@gmail.com>
2021-05-02 18:23:11 +0200

    Basic validator types.

---
+++ ConsoleTools/Validation/AndValidator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace ConsoleTools.Validation
+{
+    public class AndValidator<T> : IValidator<T>
+    {
+        public static IValidator<T> Empty => new AndValidator<T>(ImmutableList<IValidator<T>>.Empty);
+
+        public static IValidator<T> Create(IEnumerable<IValidator<T>> validators) => new AndValidator<T>(validators.ToImmutableList());
+        public static IValidator<T> Create(params IValidator<T>[] validators) => new AndValidator<T>(validators.ToImmutableList());
+
+        private readonly IImmutableList<IValidator<T>> _validators;
+
+        public AndValidator(IImmutableList<IValidator<T>> validators)
+        {
+            _validators = validators ?? throw new ArgumentNullException(nameof(validators));
+
+            if (validators.Any(v => v is null))
+                throw new ArgumentNullException(nameof(validators), "A validator method cannot be null.");
+
+            for (int i = 0; i < _validators.Count; i++)
+            {
+                while (_validators[i] is AndValidator<T> comp)
+                {
+                    _validators = _validators
+                        .RemoveAt(i)
+                        .InsertRange(i, comp._validators);
+                }
+            }
+        }
+
+        public Message Validate(T item)
+        {
+            return _validators
+                .Select(v => v.Validate(item))
+                .FirstOrDefault(m => m.IsError) ?? Message.NoError;
+        }
+    }
+}
---
+++ ConsoleTools/Validation/Validator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleTools.Validation
+{
+    public class Validator<T> : IValidator<T>
+    {
+        public static IValidator<T> NoRules => AndValidator<T>.Empty;
+
+        public static IValidator<T> Create(Func<T, Message> validator) => new Validator<T>(validator);
+
+        private readonly Func<T, Message> _validator;
+
+        public Validator(Func<T, Message> validator)
+        {
+            _validator = validator ?? throw new ArgumentNullException(nameof(validator));
+        }
+
+        public Message Validate(T item) => _validator(item);
+    }
+}
